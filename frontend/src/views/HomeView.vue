ecc651f72dc8f439184b4912c058207430968ef6
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-02 15:21:57 +0800

    chore: setup tailwindcss; remove initial file

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-red-500">Hello</h1>
   </main>
 </template>
