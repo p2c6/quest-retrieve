b909c61f85ae6192e1b925aa5e413815c9437df1
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-03 17:40:47 +0800

    feat: add temporary route param

--- frontend/src/components/ItemToRetrieve.vue
+++ frontend/src/components/ItemToRetrieve.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Status from '@/components/Status.vue';
+import { RouterLink } from 'vue-router';
 
 
 </script>
@@ -7,7 +8,7 @@ import Status from '@/components/Status.vue';
 <template>
     <div class="p-3">
         <div class="grid grid-cols-2 gap-38 place-content-between">
-            <h1 class="font-bold text-primary place-self-start text-md md:text-lg">Iphone SE</h1>
+            <h1 class="font-bold text-primary place-self-start text-md md:text-lg">Iphone XR</h1>
             <Status />
             <div class="mt-4">
                 <div class="flex flex-row items-center gap-1 text-tertiary text-sm md:text-md">
@@ -18,8 +19,13 @@ import Status from '@/components/Status.vue';
                     <i class="pi pi-calendar"></i>
                     <p>August 31, 2024</p>
                 </div>
-                <div>
-                    <button class="bg-secondary mt-5 px-4 py-1 rounded text-white text-sm">Contact</button>
+                <div class="mt-5">
+                    <RouterLink 
+                        class="bg-secondary px-4 py-1 rounded text-white text-sm"
+                        :to="{name: 'contact', params: {id: 1}}"
+                    >
+                        Contact
+                    </RouterLink>
                 </div>
             </div>
         </div>
