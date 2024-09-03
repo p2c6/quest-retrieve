31e3d2d21b9e7ab500d7e315d341823e567f33ba
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-03 15:17:53 +0800

    feat: add card list component

---
+++ frontend/src/components/CardList.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container mx-auto mt-2">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+            <slot></slot>
+        </div>
+    </div>
+</template>
