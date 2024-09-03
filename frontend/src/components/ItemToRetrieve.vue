e1c03266e9a49b80e3a5eafbe75825bf087980a9
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-03 15:18:24 +0800

    feat: add item to retrieve component

---
+++ frontend/src/components/ItemToRetrieve.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Status from '@/components/Status.vue';
+
+
+</script>
+
+<template>
+    <div class="p-3">
+        <div class="grid grid-cols-2 gap-38 place-content-between">
+            <h1 class="font-bold text-primary place-self-start text-md">Iphone SE</h1>
+            <Status />
+            <div class="mt-4">
+                <div class="flex flex-row items-center gap-1">
+                    <i class="text-indigo-800 pi pi-map-marker"></i>
+                    <p class="text-xs">Quezon City</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
