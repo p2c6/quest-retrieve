dd63a6e688a31be6a3be4c00c6fd0136a9db70a9
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-03 17:40:02 +0800

    feat: add link on logo

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <script setup>
 import logo from "@/assets/qr-logo.png";
 import { ref } from "vue";
+import { RouterLink } from "vue-router";
 
 const collapse = ref(false);
 
@@ -15,7 +16,9 @@ const collapseNav = () => collapse.value = !collapse.value;
                 <button class="mt-2 border-[1.7px] rounded border-indigo-800 px-2 py-1 md:hidden" @click="collapseNav" id="btnCollapseNav">
                     <i class="text-indigo-800 pi pi-bars"></i>
                 </button>
-                <img :src="logo" alt="QuestRetrieve Logo" width="250px">
+                <RouterLink :to="{name: 'home'}">
+                    <img :src="logo" alt="QuestRetrieve Logo" width="250px">
+                </RouterLink>
             </div>
 
         </div>
