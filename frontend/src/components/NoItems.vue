7ea51432b67784ef98df43e7e38749a2dc76f9a2
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-03 16:12:09 +0800

    feat: add no item component

---
+++ frontend/src/components/NoItems.vue
@@ -0,0 +1,12 @@
+<script setup>
+import emptyIllustration from "@/assets/empty_item.png";
+</script>
+
+<template>
+    <main class="grid grid-1 justify-items-center">
+        <div class="mt-5 flex flex-col items-center">
+            <img :src="emptyIllustration" alt="No Items Image" class="md:w-120 md:h-120" />
+            <p class="text-tertiary">Ooops. There's no item found. Please try again...</p>
+        </div>
+    </main>
+</template>
