30d9eeab3d4dc0811fc40320e9fc33432f5aa1ff
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-03 16:11:42 +0800

    style: add padding on different screen

--- frontend/src/components/CardList.vue
+++ frontend/src/components/CardList.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="container mx-auto p-2 md:p-0 mt-2">
+    <div class="container mx-auto p-2 md:p-2 lg:p-0 mt-2">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
             <slot></slot>
         </div>
