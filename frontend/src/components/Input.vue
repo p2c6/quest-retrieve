02fe3729ff6b08fc4637ccdaf136f171628b02b3
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-05 15:01:54 +0800

    style: add padding

--- frontend/src/components/Input.vue
+++ frontend/src/components/Input.vue
@@ -3,6 +3,6 @@
 
 <template>
     <div>
-        <input type="text" class="h-8 w-full border-[1.1px] border-primary mt-2 mb-2 rounded">
+        <input type="text" class="h-8 w-full border-[1.1px] border-primary mt-2 mb-2 p-2 rounded">
     </div>
 </template>
