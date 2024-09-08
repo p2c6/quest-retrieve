05c8a7a66613c5d1590429ab15efe57683fd0095
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-08 13:57:40 +0800

    feat: add migration modal; update column

---
+++ backend/database/migrations/2024_09_08_055351_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('subcategory_id');
+            $table->string('name')->nullable();
+            $table->string('incident_location', 255);
+            $table->date('incident_date');
+            $table->date('finish_transaction_date');
+            $table->date('expiration_date');
+            $table->string('status', 30);
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
