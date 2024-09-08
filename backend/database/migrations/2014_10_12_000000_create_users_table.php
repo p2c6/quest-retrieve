8edd21881f96a716f78272dd427e35a85e30c354
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-08 13:45:48 +0800

    feat: update role id data type

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->tinyInteger('role_id');
+            $table->unsignedBigInteger('role_id');
             $table->rememberToken();
             $table->timestamps();
-            
+
             $table->foreign('role_id')->references('id')->on('ranks');
         });
     }
