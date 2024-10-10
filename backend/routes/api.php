c0826ab0b70bb462e0447e54ff480f7b7e9c042f
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-11 04:45:21 +0800

    feat: add route for login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
+use App\Http\Controllers\API\v1\Authentication\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::prefix('/v1')->name('api.v1.')->group(function() {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    //Authentication
+    Route::prefix('authentication')->name('authentiaction.')->group(function() {
+        Route::post('/login', [LoginController::class, 'login'])->name('login');
+    });
+
 });
