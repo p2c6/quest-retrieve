a5971c94b52d45b0c82098e81f108bfc9cf51c64
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-11 05:10:30 +0800

    feat: add invalid credential test

--- backend/tests/Feature/Authentication/LoginTest.php
+++ backend/tests/Feature/Authentication/LoginTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Authentication;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -33,9 +34,15 @@ class LoginTest extends TestCase
     public function test_user_can_login_successfully(): void
     {
         try {
+            $role = Role::where('name', 'Admin')->first();
+
+            if (!$role) {
+                $this->fail('Role Admin not found in the database.');
+            }
+
             $user = User::factory()->create([
                 'password' => bcrypt('password123'),
-                'role_id' => 1
+                'role_id' => $role->id
             ]);
 
             $csrf = $this->get('/sanctum/csrf-cookie');
@@ -52,6 +59,45 @@ class LoginTest extends TestCase
                       ->assertJsonStructure(['user', 'message'])
                       ->assertJson(['message' => 'Successfully logged in.'])
                       ->assertSee('user');
+
+
+        } catch (\Exception $e) {
+            $this->fail('An error occured' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Test user cannot login with invalid credentials via API.
+     * 
+     * This test verifies that a user cannot be logged-in with invalid credentials via API endpoint.
+     */
+    public function test_user_cannot_login_with_invalid_credentials(): void
+    {
+        try {
+            $role = Role::where('name', 'Admin')->first();
+
+            if (!$role) {
+                $this->fail('Role Admin not found in the database.');
+            }
+
+            $user = User::factory()->create([
+                'password' => bcrypt('password123'),
+                'role_id' => $role->id
+            ]);
+
+            $csrf = $this->get('/sanctum/csrf-cookie');
+
+            $csrf->assertCookie('XSRF-TOKEN');
+
+            $response = $this->post('/api/v1/authentication/login', [
+                'email' => $user->email,
+                'password' => 'password1234',
+            ]);
+
+            $response->assertCookie('laravel_session')
+                      ->assertStatus(401)
+                      ->assertJsonStructure(['message'])
+                      ->assertJson(['message' => 'The provided credentials are incorrect.']);
 
 
         } catch (\Exception $e) {
