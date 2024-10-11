e97c32b949612f489d5af281e1b6b210dddd0501
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-12 03:27:54 +0800

    feat: add logout route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\v1\Authentication\LoginController;
+use App\Http\Controllers\API\V1\Authentication\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,7 @@ Route::prefix('/v1')->name('api.v1.')->group(function() {
     //Authentication
     Route::prefix('authentication')->name('authentiaction.')->group(function() {
         Route::post('/login', [LoginController::class, 'login'])->name('login');
+        Route::post('/logout', [LogoutController::class, 'logout'])->name('logout')->middleware('web');
     });
 
 });
