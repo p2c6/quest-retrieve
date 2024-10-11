9a63027508ee731e5b9a387a73ee40993d1cdf1f
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-12 01:28:23 +0800

    chore: update docblock

--- backend/app/Http/Controllers/API/v1/Authentication/LoginController.php
+++ backend/app/Http/Controllers/API/v1/Authentication/LoginController.php
@@ -4,18 +4,34 @@ namespace App\Http\Controllers\API\v1\Authentication;
 
 use App\Http\Controllers\Controller;
 use App\Services\Authentication\LoginService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    /**
+     * The login service instance.
+     * 
+     * @var LoginService
+     */
     private $service;
-
+    
+    /**
+     * LoginController contructor.
+     * 
+     * @param LoginService $service The instance of LoginService.
+     */
     public function __construct(LoginService $service)
     {
         $this->service = $service;
     }
 
-    public function login(Request $request)
+    /**
+     * Handle user log-in request.
+     * 
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request): JsonResponse
     {
         return $this->service->login($request);
     }
