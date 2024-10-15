e4057538c136b26472b83c3f827f3484c420f98b
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-15 23:56:12 +0800

    chore: remove unuse import

--- backend/app/Http/Controllers/API/v1/Authentication/LoginController.php
+++ backend/app/Http/Controllers/API/v1/Authentication/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API\v1\Authentication;
 
 use App\Http\Controllers\Controller;
 use App\Services\Authentication\LoginService;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
