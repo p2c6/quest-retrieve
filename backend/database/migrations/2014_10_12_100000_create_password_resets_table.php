aa5d8831b2454e66df82f378806fd36e487b3c0b
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-22 05:15:51 +0800

    chore: update table name

--- backend/database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ backend/database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -11,18 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
+        Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
     }
 
+
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('password_resets');
     }
 };
