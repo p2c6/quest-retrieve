01129202512666d9e4220dd0ffd9b330ca19ec69
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-24 05:50:50 +0800

    feat: add fillable column

--- backend/app/Models/Role.php
+++ backend/app/Models/Role.php
@@ -10,6 +10,10 @@ class Role extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name'
+    ];
+
     public function users(): HasMany
     {
         return $this->hasMany(User::class);
