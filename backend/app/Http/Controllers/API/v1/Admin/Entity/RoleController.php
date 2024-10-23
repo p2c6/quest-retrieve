63903dc6f34c1c178facfbbb82bc9636fada72e6
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-24 05:51:21 +0800

    chore: move directory

---
+++ backend/app/Http/Controllers/API/v1/Admin/Entity/RoleController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Admin\Entity;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RoleCollection;
+use App\Http\Resources\RoleResource;
+use App\Models\Role;
+use App\Services\Entity\Role\RoleService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    /**
+     * The RoleService instance.
+     * 
+     * @var RoleService
+     */
+    private $service;
+
+    /**
+     * The RoleController constructor.
+     * 
+     * @param RoleService $service The instance of RoleService
+     */
+    public function __construct(RoleService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Handle show role.
+     * 
+     * @param App\Models\Role $role The model of the role which needs to be retrieved.
+     * @return App\Http\Resources\RoleResource
+     */
+    public function index(): RoleCollection
+    {
+        return new RoleCollection(Role::paginate());
+    }
+
+
+    /**
+     * Handle show role.
+     * 
+     * @param App\Models\Role $role The model of the role which needs to be retrieved.
+     * @return App\Http\Resources\RoleResource
+     */
+    public function show(Role $role): RoleResource
+    {
+        return new RoleResource(Role::findOrFail($role->id));
+    }
+
+    /**
+     * Handle store role request.
+     * 
+     * @param Illuminate\Http\Request $request The HTTP request object containing role data.
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        return $this->service->store($request);
+    }
+
+    /**
+     * Handle update role request.
+     * 
+     * @param App\Models\Role $role The model of the role which needs to be updated.
+     * @param \Illuminate\Http\Request $request The HTTP request object containing role data.
+     * 
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function update(Role $role, Request $request): JsonResponse
+    {
+        return $this->service->update($role, $request);
+    }
+
+    /**
+     * Handle delete role.
+     * 
+     * @param App\Models\Role $role The model of the role which needs to be deleted.
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function delete(Role $role): JsonResponse
+    {
+        return $this->service->delete($role);
+    }
+}
