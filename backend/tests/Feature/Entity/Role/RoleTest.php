425d54a3f0a5107ee51624b6b1c0f77897f0bbc2
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-24 07:11:10 +0800

    feat: add test for update role with valid input

--- backend/tests/Feature/Entity/Role/RoleTest.php
+++ backend/tests/Feature/Entity/Role/RoleTest.php
@@ -8,6 +8,8 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+use function PHPUnit\Framework\assertNotEquals;
+
 class RoleTest extends TestCase
 {
     use RefreshDatabase;
@@ -166,5 +168,52 @@ class RoleTest extends TestCase
                 ->assertJson([
                     'message' => 'Unauthenticated.', 
                 ]);
+    }
+
+    /**
+     * Test user can update role with valid inputs via API.
+     * 
+     * This test verifies that a user can update role with valid inputs via API endpoint.
+     */
+    public function test_user_can_update_role_with_valid_inputs(): void
+    {
+        $role = Role::where('id', UserType::PUBLIC_USER)->first();
+
+        if (!$role) {
+            $this->fail('Role Public User not found in the database.');
+        }
+
+        $user = User::factory()->create([
+            'password' => bcrypt('password123'),
+            'role_id' => $role->id,
+        ]);
+
+        $this->get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN');
+
+        $this->post('/api/v1/authentication/login', [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+
+        $newRoleName = 'Old Role';
+        $updatedRoleName = 'New Role';
+
+        $newRole = Role::create([
+            'name' => $newRoleName
+        ]);
+
+        $response = $this->putJson(route('api.v1.roles.update', $newRole->id), [
+            'name' => $updatedRoleName
+        ]);
+
+        $updatedRole = Role::find($newRole->id);
+
+        $response->assertCookie('laravel_session')
+                ->assertStatus(200)
+                ->assertJson(['message' => 'Successfully Role Updated.']);
+
+        $this->assertNotEquals($newRoleName, $updatedRole->name, 'Values should not be equals');
     }
 }
