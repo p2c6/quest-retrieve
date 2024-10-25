76e24f897a929aa07dc4af6d100c0dcac6903e82
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-26 01:13:23 +0800

    feat: add subcategory cannot store store subcategory with subcategory name more than 100 char

--- backend/tests/Feature/Entity/SubCategory/SubCategoryTest.php
+++ backend/tests/Feature/Entity/SubCategory/SubCategoryTest.php
@@ -202,4 +202,49 @@ class SubCategoryTest extends TestCase
                 ]);
     }
     
+    /**
+     * Test user cannot store subcategory with subcategory name more than 100 hundred characters via API.
+     * 
+     * This test verifies that a user cannot store subcategory with subcategory name more than 100 characters via API endpoint.
+     */
+    public function test_user_cannot_store_subcategory_with_subcategory_name_more_than_one_hundred_chars(): void
+    {
+        $role = Role::where('id', UserType::PUBLIC_USER)->first();
+
+        if (!$role) {
+            $this->fail('Role Public User not found in the database.');
+        }
+
+        $user = User::factory()->create([
+            'password' => bcrypt('password123'),
+            'role_id' => $role->id,
+        ]);
+
+        $this->get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN');
+
+        $this->post('/api/v1/authentication/login', [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+
+        $category = Category::create([
+            'name' => "Sample Category"
+        ]);
+
+        $response = $this->postJson('/api/v1/subcategories', [
+            'category_id' => $category->id,
+            'name' =>  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit incidunt at accusantium. Ex cumque facilis repudiandae eum laudantium sed fuga dolor placeat laborum neque, fugit quia magni? Cupiditate magni corporis error ipsa perferendis soluta velit aperiam ab laborum nemo officia et eum maxime sapiente consequuntur quasi, vero sunt? Suscipit ad qui numquam amet ratione, porro perspiciatis quaerat sequi voluptate vel? Corporis hic optio, voluptatem suscipit quae cupiditate officia dignissimos. Tenetur maxime eos atque itaque quis! Repellat, maiores. Asperiores ullam neque illum perferendis corporis tempora labore ex error architecto debitis sunt laudantium, totam rem repellendus aut! Assumenda, dolore magnam nisi modi corporis quod dolorum eligendi aspernatur!"
+        ]);
+
+        $response->assertStatus(422)
+                ->assertJsonStructure(['message', 'errors'])
+                ->assertJson([
+                    'message' => 'The name field must not be greater than 100 characters.', 
+                    'errors' => [
+                        'name' => ['The name field must not be greater than 100 characters.']
+                    ]
+                ]);
+    }
 }
