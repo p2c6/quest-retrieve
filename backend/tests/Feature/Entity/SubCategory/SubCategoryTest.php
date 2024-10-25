8c0ffc1bd55d27f46f5b51cf6c8bdcb55c0639fc
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-26 01:06:17 +0800

    feat: add subcategory cannot store store subcategory with empty category test

--- backend/tests/Feature/Entity/SubCategory/SubCategoryTest.php
+++ backend/tests/Feature/Entity/SubCategory/SubCategoryTest.php
@@ -69,6 +69,49 @@ class SubCategoryTest extends TestCase
     }
 
     /**
+     * Test user cannot store subcategory with empty category inputs via API.
+     * 
+     * This test verifies that a user cannot store subcategory with empty category via API endpoint.
+     */
+    public function test_user_cannot_store_subcategory_with_empty_category(): void
+    {
+        $role = Role::where('id', UserType::PUBLIC_USER)->first();
+
+        if (!$role) {
+            $this->fail('Role Public User not found in the database.');
+        }
+
+        $user = User::factory()->create([
+            'password' => bcrypt('password123'),
+            'role_id' => $role->id,
+        ]);
+
+        $this->get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN');
+
+        // Log in the user
+        $this->post('/api/v1/authentication/login', [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+
+        $response = $this->postJson('/api/v1/subcategories', [
+            'category_id' => '',
+            'name' => 'Sub Category 1'
+        ]);
+
+        $response->assertStatus(422)
+                ->assertJsonStructure(['message', 'errors'])
+                ->assertJson([
+                    'message' => 'The category field is required.', 
+                    'errors' => [
+                        'category_id' => ['The category field is required.']
+                    ]
+                ]);
+    }
+
+    /**
      * Test user cannot store subcategory with empty subcategory name inputs via API.
      * 
      * This test verifies that a user cannot store subcategory with empty subcategory name via API endpoint.
