f63876cf61cb29d9f043dcbed51f9eb64adbdca8
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-29 07:41:57 +0800

    feat: add fillable column

--- backend/app/Models/Profile.php
+++ backend/app/Models/Profile.php
@@ -10,6 +10,17 @@ class Profile extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'last_name',
+        'first_name',
+        'birthday',
+        'contact_no',
+        'avatar',
+        'profile_date_updated',
+        
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
