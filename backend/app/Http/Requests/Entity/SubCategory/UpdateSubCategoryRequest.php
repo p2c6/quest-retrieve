ee2ca05134d4be5bdfff47bfca7469d56688a538
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-28 08:59:41 +0800

    feat: add store subcategory form request validator; add update subcategory form request validator

---
+++ backend/app/Http/Requests/Entity/SubCategory/UpdateSubCategoryRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Entity\SubCategory;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateSubCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'category_id' => 'required',
+            'name' => 'required|max:100',
+        ];
+    }
+
+    public function messages(): array
+    {
+        $messages = ['category_id' => 'The category field is required.'];
+
+        return $messages;
+    }
+}
