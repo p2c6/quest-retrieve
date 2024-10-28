7457dcb99f2f476eec762b035fa3a35fa2198a17
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-28 09:37:52 +0800

    feat: update return structure

--- backend/app/Http/Resources/Entity/SubCategory/SubCategoryResource.php
+++ backend/app/Http/Resources/Entity/SubCategory/SubCategoryResource.php
@@ -14,6 +14,10 @@ class SubCategoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'category_id' => $this->category_id,
+            'name' => $this->name,
+        ];
     }
 }
