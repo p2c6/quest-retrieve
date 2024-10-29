7d1036759f5c4f02902af795fbefbb24430135f6
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-29 08:30:29 +0800

    feat: add update profile form validator request

---
+++ backend/app/Http/Requests/UserProfile/UpdateUserProfileRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\UserProfile;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'last_name' => 'required',
+            'first_name'  => 'required',
+            'birthday' => 'required|date',
+            'contact_no' => 'required',
+        ];
+    }
+}
