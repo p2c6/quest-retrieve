aa798f0ff8a1df0089c455be077d0bedba883e0e
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-30 20:29:08 +0800

    feat: add post initial test

---
+++ backend/tests/Feature/Post/PostTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\Feature\Post;
+
+use App\Enums\PostType;
+use App\Enums\UserType;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Role;
+use App\Models\Subcategory;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Seed role.
+     * 
+     * Set up the test environment by seeding the database with roles.
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->artisan('db:seed', ['--class' => 'RoleSeeder']);
+        $this->artisan('db:seed', ['--class' => 'UserSeeder']);
+        $this->artisan('db:seed', ['--class' => 'CategorySeeder']);
+        $this->artisan('db:seed', ['--class' => 'SubCategorySeeder']);
+        $this->artisan('db:seed', ['--class' => 'PostSeeder']);
+    }
+
+    /**
+     * Test user can post with valid inputs via API.
+     * 
+     * This test verifies that a user can post with valid inputs via API endpoint.
+     */
+    public function test_user_can_store_post_with_valid_inputs(): void
+    {
+        $role = Role::where('id', UserType::PUBLIC_USER)->first();
+
+        if (!$role) {
+            $this->fail('Role Public User not found in the database.');
+        }
+
+        $this->get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN');
+
+        
+        $user = User::factory()->create([
+            'password' => bcrypt('password123'),
+            'role_id' => $role->id,
+        ]);
+
+        $this->get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN');
+
+        $this->post('/api/v1/authentication/login', [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+
+        $category = Category::create([
+            'name' => "Sample Category",
+        ]);
+
+        $subCategory = Subcategory::create([
+            'category_id' => $category->id,
+            'name' => "Sample Subcategory",
+        ]);
+
+        $subCategoryId = $subCategory->id;
+        
+        $response = $this->postJson(route('api.v1.posts.store'), [
+            'user_id' => $user->id,
+            'type' => PostType::LOST,
+            'subcategory_id' => $subCategoryId,
+            'incident_location' => 'Quezon City',
+            'incident_date' => '2024-05-06',
+            'finish_transaction_date' => '2024-05-07',
+            'expiration_date' =>  '2024-06-06',
+            'status' => 'finish',
+        ]);
+
+        $response->assertCookie('laravel_session')
+                ->assertStatus(201)
+                ->assertJsonStructure(['message'])
+                ->assertJson(['message' => 'Successfully Post Created.']);
+    }
+}
