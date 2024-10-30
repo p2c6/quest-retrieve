6dc3beec37143bd7f3bc799dec8a2be2a47426cc
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-30 17:58:34 +0800

    feat: add post service; add store post functionality

---
+++ backend/app/Services/Post/PostService.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Services\Post\PostService;
+
+use App\Enums\PostStatus;
+use App\Enums\PostType;
+use App\Enums\UserType;
+use App\Models\Post;
+use App\Models\User;
+use App\Services\Contracts\Authentication\RegisterInterface;
+use App\Services\UserProfile\UserProfileService;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
+
+class PostService
+{
+    /**
+     * Handle user store post request.
+     * 
+     * @param \Illuminate\Http\Request $request The HTTP request object containing user data.
+     * 
+     * @return mixed
+     */
+    public function store($request) : JsonResponse
+    {
+        try {
+            
+            Post::create([
+                'user_id' => $request->user()->id,
+                'type' => $request->type,
+                'subcategory_id' => $request->subcategory_id,
+                'incident_location' => $request->incident_location,
+                'incident_date' => $request->incident_date,
+                'finish_transaction_date' => $request->finish_transaction_date,
+                'expiration_date' =>  $request->expiration_date,
+                'status' => PostStatus::PENDING->value,
+            ]);
+            
+            
+            return response()->json([
+                'message' => 'Successfully Post Created.'
+            ], 201);
+        } catch (ValidationException $validationException) {
+            info("Validation Error on storing post: " . $validationException->getMessage());
+            return response()->json(['errors' => $validationException->errors()], 422);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            info("Error on user register: " . $th->getMessage());
+            return response()->json([
+                'message' => 'An error occurred during storing post.'
+            ], 500);
+        }
+    }
+}
