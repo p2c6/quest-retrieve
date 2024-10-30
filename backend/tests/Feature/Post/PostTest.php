92270dd6459a304b68fa72f479c41d032af4ec6e
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-30 20:56:36 +0800

    feat: add test user can update with all valid input

--- backend/tests/Feature/Post/PostTest.php
+++ backend/tests/Feature/Post/PostTest.php
@@ -204,4 +204,79 @@ class PostTest extends TestCase
                     'message' => 'Unauthenticated.', 
                 ]);
     }
+
+    /**
+     * Test user can update post with valid inputs via API.
+     * 
+     * This test verifies that a user can update post with valid inputs via API endpoint.
+     */
+    public function test_user_can_update_post_with_valid_inputs(): void
+    {
+        $role = Role::where('id', UserType::PUBLIC_USER)->first();
+
+        if (!$role) {
+            $this->fail('Role Public User not found in the database.');
+        }
+
+        $user = User::factory()->create([
+            'password' => bcrypt('password123'),
+            'role_id' => $role->id
+        ]);
+
+        $csrf = $this->get('/sanctum/csrf-cookie');
+
+        $csrf->assertCookie('XSRF-TOKEN');
+
+        $this->postJson('/api/v1/authentication/login', [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+
+        $category = Category::create([
+            'name' => "Sample Category",
+        ]);
+
+        $subCategory = Subcategory::create([
+            'category_id' => $category->id,
+            'name' => "Sample Subcategory",
+        ]);
+
+        $originalType = "Lost";
+        $originalSubCategoryId = $subCategory->id;
+        $originalIncidentLocation = 'Manila City';
+        $originalIncidentDate = '2024-01-02';
+
+        $post = Post::create([
+            'user_id' => $user->id,
+            'type' => $originalType,
+            'subcategory_id' => $originalSubCategoryId,
+            'incident_location' => $originalIncidentLocation,
+            'incident_date' => $originalIncidentDate,
+            'status' => PostStatus::PENDING
+        ]);
+
+        $updatedType = "Found";
+        $updatedSubCategoryId = 2;
+        $updatedIncidentLocation = 'Quezon City';
+        $updatedIncidentDate = '2024-05-02';
+
+        $response = $this->putJson(route('api.v1.posts.update', $post->id), [
+            'user_id' => $user->id,
+            'type' => $updatedType,
+            'subcategory_id' => $updatedSubCategoryId,
+            'incident_location' => $updatedIncidentLocation,
+            'incident_date' => $updatedIncidentDate,
+        ]);
+
+        $this->assertNotSame($originalType, $updatedType, 'Type must not equal to previous');
+        $this->assertNotSame($originalSubCategoryId, $updatedSubCategoryId, 'Subcategory must not equal to previous');
+        $this->assertNotSame($originalIncidentLocation, $updatedIncidentLocation, 'Incident Location must not equal to previous');
+        $this->assertNotSame($originalIncidentDate, $updatedIncidentDate, 'Incident Date must not equal to previous');
+
+        $response->assertStatus(200)
+                ->assertJsonStructure(['message'])
+                ->assertJson([ 'message' => 'Successfully Post Updated.']);
+    }
 }
