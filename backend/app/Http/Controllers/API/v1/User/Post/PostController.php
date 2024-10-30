6de41374e7437997e595569edfc1c236649e03a6
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-30 19:09:01 +0800

    feat: implement update post

--- backend/app/Http/Controllers/API/v1/User/Post/PostController.php
+++ backend/app/Http/Controllers/API/v1/User/Post/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\API\v1\User\Post;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Post\StorePostRequest;
+use App\Http\Requests\Post\UpdatePostRequest;
+use App\Models\Post;
 use App\Services\Post\PostService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -36,5 +38,16 @@ class PostController extends Controller
     public function store(StorePostRequest $request): JsonResponse
     {
         return $this->service->store($request);
+    }
+
+    /**
+     * Handle update post request.
+     * 
+     * @param App\Http\Requests\Post\UpdatePostRequest $request The HTTP request object containing role data.
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function update(Post $post, UpdatePostRequest $request): JsonResponse
+    {
+        return $this->service->update($post, $request);
     }
 }
