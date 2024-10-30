78bf91d3974176fdd3655d801f5a7f325fd0aae8
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-30 16:27:21 +0800

    feat: add column

--- backend/database/migrations/2024_09_08_055351_create_posts_table.php
+++ backend/database/migrations/2024_09_08_055351_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->string('type', 10);
             $table->unsignedBigInteger('subcategory_id');
             $table->string('name')->nullable();
             $table->string('incident_location', 255);
