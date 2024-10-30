e9c85b75d928ac40b7f3efc9563d931fb2b6926e
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-30 19:33:15 +0800

    feat: add relationship

--- backend/app/Models/Subcategory.php
+++ backend/app/Models/Subcategory.php
@@ -13,4 +13,9 @@ class Subcategory extends Model
         'category_id',
         'name'
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
