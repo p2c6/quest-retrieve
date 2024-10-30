20181299034d292a5772f48cf10e34213842269f
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-30 18:54:40 +0800

    feat: add store post request validator

---
+++ backend/app/Http/Requests/Post/StorePostRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'subcategory_id' => 'required|exists:subcategories,id', 
+            'type' => 'required|string', 
+            'incident_location' => 'required|string',
+            'incident_date' => 'required|date', 
+            'finish_transaction_date' => 'required|date', 
+        ];
+    }
+}
