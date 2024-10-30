f3f54096b1597624664cc6b30954d2c9ea01d217
Archie Jefferson Em <archie.em99@gmail.com>
2024-10-30 17:17:35 +0800

    feat: add post controller

---
+++ backend/app/Http/Controllers/API/v1/Post/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Post\PostController;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserProfile\UpdateUserProfileRequest;
+use App\Models\Profile;
+use App\Models\User;
+use App\Services\UserProfile\UserProfileService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+}
