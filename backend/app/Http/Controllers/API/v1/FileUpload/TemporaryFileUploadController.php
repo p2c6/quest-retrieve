90ed675f6bb0d94d49f8d7a1fc3cb0a9b855dd92
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-01 07:59:20 +0800

    feat: add temporary file upload controller; implement temporary file upload service

---
+++ backend/app/Http/Controllers/API/v1/FileUpload/TemporaryFileUploadController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\FileUpload;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\FileUpload\TemporaryFileRevertRequest;
+use App\Http\Requests\FileUpload\TemporaryFileUploadRequest;
+use App\Http\Requests\UserProfile\UpdateUserProfileRequest;
+use App\Models\Profile;
+use App\Models\User;
+use App\Services\FileUpload\TemporaryFileUploadService;
+use App\Services\UserProfile\UserProfileService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TemporaryFileUploadController extends Controller
+{
+    /**
+     * The temporary file upload service instance.
+     * 
+     * @var TemporaryFileUploadService
+     */
+    private $service;
+    
+    /**
+     * TemporaryFileUploadController contructor.
+     * 
+     * @param TemporaryFileUploadService $service The instance of TemporaryFileUploadService.
+     */
+    public function __construct(TemporaryFileUploadService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Handle user temporary file upload request.
+     * 
+     * @param App\Http\Requests\FileUpload\TemporaryFileUploadRequest $request The HTTP request object containing user data.
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function upload(TemporaryFileUploadRequest $request): JsonResponse
+    {
+        return $this->service->upload($request);
+    }
+
+    /**
+     * Handle user revert temporary uploaded file request.
+     * 
+     * @param  App\Http\Requests\FileUpload\TemporaryFileRevertRequest $request The HTTP request object containing user data.
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function revert(TemporaryFileRevertRequest $request): JsonResponse
+    {
+        return $this->service->revert($request);
+    }
+}
