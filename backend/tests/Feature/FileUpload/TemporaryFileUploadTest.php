e2dafdae661fd34b73ece413be23c5d2c1db58f9
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-01 08:44:08 +0800

    feat: add test user cannot upload temporary file with empty field; add test user cannot upload temporary file if not image

--- backend/tests/Feature/FileUpload/TemporaryFileUploadTest.php
+++ backend/tests/Feature/FileUpload/TemporaryFileUploadTest.php
@@ -72,4 +72,88 @@ class TemporaryFileUploadTest extends TestCase
         $response->assertStatus(200)
                     ->assertJsonStructure(['file_path']);
     }
+
+    /**
+     * Test user cannot upload temporary file with empty field via API.
+     * 
+     * This test verifies that a user cannot upload temporary file with empty field  via API endpoint.
+     */
+    public function test_user_cannot_upload_temporary_file_with_empty_field(): void
+    {
+        $role = Role::where('id', UserType::PUBLIC_USER)->first();
+
+        if (!$role) {
+            $this->fail('Role Public User not found in the database.');
+        }
+
+        $user = User::factory()->create([
+            'password' => bcrypt('password123'),
+            'role_id' => $role->id
+        ]);
+
+        $csrf = $this->get('/sanctum/csrf-cookie');
+
+        $csrf->assertCookie('XSRF-TOKEN');
+
+        $response = $this->postJson('/api/v1/authentication/login', [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+
+        $response = $this->postJson(route('api.v1.temporary-file.upload'),  [
+            'file' => ''
+        ]);
+
+        $response->assertStatus(422)
+                    ->assertJsonStructure(['message', 'errors'])
+                    ->assertJson(['errors' => [
+                        'file' => ['The file field is required.']
+                    ]])
+                    ->assertJsonValidationErrors(['file']);
+    }
+
+    /**
+     * Test user cannot upload temporary file if not image field via API.
+     * 
+     * This test verifies that a user cannot upload temporary file if not image via API endpoint.
+     */
+    public function test_user_cannot_upload_temporary_file_if_not_image(): void
+    {
+        $role = Role::where('id', UserType::PUBLIC_USER)->first();
+
+        if (!$role) {
+            $this->fail('Role Public User not found in the database.');
+        }
+
+        $user = User::factory()->create([
+            'password' => bcrypt('password123'),
+            'role_id' => $role->id
+        ]);
+
+        $csrf = $this->get('/sanctum/csrf-cookie');
+
+        $csrf->assertCookie('XSRF-TOKEN');
+
+        $response = $this->postJson('/api/v1/authentication/login', [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+
+        $file = UploadedFile::fake()->create('example.pdf', 100);
+
+        $response = $this->postJson(route('api.v1.temporary-file.upload'),  [
+            'file' => $file
+        ]);
+
+        $response->assertStatus(422)
+                    ->assertJsonStructure(['message', 'errors'])
+                    ->assertJson(['errors' => [
+                        'file' => ['The file field must be a file of type: jpg, jpeg, png.']
+                    ]])
+                    ->assertJsonValidationErrors(['file']);
+    }
 }
