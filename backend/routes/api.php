8fac714fa1bbdd51eb87773cc6fdcf051b55cd29
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-04 19:19:34 +0800

    feat: add route for post filter

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\v1\Admin\Entity\Category\CategoryController;
 use App\Http\Controllers\API\v1\Admin\Entity\Role\RoleController;
 use App\Http\Controllers\API\v1\Admin\Entity\SubCategory\SubCategoryController;
+use App\Http\Controllers\API\v1\Approval\ApprovalController;
 use App\Http\Controllers\API\v1\Authentication\LoginController;
 use App\Http\Controllers\API\V1\Authentication\LogoutController;
 use App\Http\Controllers\API\v1\Authentication\RegisterController;
@@ -27,11 +28,14 @@ Route::prefix('/v1')->name('api.v1.')->group(function() {
     Route::middleware('auth:sanctum')->group(function() {
         //Roles
         Route::apiResource('roles', RoleController::class);
+
         //Categories
         Route::apiResource('categories', CategoryController::class);
+
         //Subcategories
         Route::apiResource('subcategories', SubCategoryController::class)
             ->parameters(['subcategories' => 'subCategory']);
+            
         //Post
         Route::apiResource('posts', PostController::class);
 
@@ -42,6 +46,11 @@ Route::prefix('/v1')->name('api.v1.')->group(function() {
         Route::prefix('temporary-file')->name('temporary-file.')->group(function() {
             Route::post('/upload', [TemporaryFileUploadController::class, 'upload'])->name('upload');
             Route::post('/revert', [TemporaryFileUploadController::class, 'revert'])->name('revert');
+        });
+
+        //For Approval
+        Route::prefix('approval/posts')->name('for-approval.')->group(function() {
+            Route::get('/', [ApprovalController::class, 'index'])->name('index');
         });
     });
 });
