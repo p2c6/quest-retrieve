266ffe8d522a5fee92ee3f77fc4244936a8f4c91
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-05 18:35:14 +0800

    feat: update implementation with spatie query builder

--- backend/app/Http/Controllers/API/v1/Approval/ApprovalController.php
+++ backend/app/Http/Controllers/API/v1/Approval/ApprovalController.php
@@ -4,47 +4,24 @@ namespace App\Http\Controllers\API\v1\Approval;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Spatie\QueryBuilder\QueryBuilder;
+use App\Filters\DateFilter;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class ApprovalController extends Controller
 {
     public function index(Request $request)
     {
-        $q = $request->query('q');
-
-        $posts = Post::with(['user' => function($query) {
-            $query->select('id');
-        }, 'user.profile' => function($query) {
-            $query->select('user_id', 'last_name', 'first_name');
-        },'subcategory' => function($query) {
-            $query->select('id', 'name', 'category_id');
-        }, 'subcategory.category' => function($query) {
-            $query->select('id', 'name');
-        },])
-        ->select(
-            'posts.user_id', 
-            'posts.type', 
-            'posts.subcategory_id', 
-            'posts.incident_location', 
-            'posts.incident_date', 
-            'posts.finish_transaction_date',
-            'posts.expiration_date',
-            'posts.status'
-        );
-
-        if ($q !== null) {
-            
-            $posts->where('posts.status', 'like', '%' . $q . '%')
-            ->orWhereHas('user.profile', function($query) use ($q) {
-                $query->where('first_name', 'like', '%' . $q . '%')
-                        ->orWhere('last_name', 'like', '%' . $q . '%')
-                        ->orWhereRaw("CONCAT(first_name, ' ', last_name) LIKE ?", ['%' . $q . '%'])
-                        ->select('first_name', 'last_name');
-            });
-        }
-        $posts = $posts->get();
+         $posts = QueryBuilder::for(Post::class)
+            ->allowedIncludes('profile')
+            ->allowedFilters([
+                AllowedFilter::custom('search', new DateFilter),
+            ])
+            ->get();
         
-        return response()->json($posts, 200);
-
+        return response()->json($posts);
     }
 }
