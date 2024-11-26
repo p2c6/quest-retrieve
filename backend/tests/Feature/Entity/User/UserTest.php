2e5b90742a4c12d04bb804d4b4b62abd41494281
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-26 16:14:01 +0800

    feat: add user test class

---
+++ backend/tests/Feature/Entity/User/UserTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature\Entity\User;
+
+use App\Enums\UserType;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+use function PHPUnit\Framework\assertNotEquals;
+
+class UserTest extends TestCase
+{
+    
+}
