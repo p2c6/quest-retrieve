e96247252fa6b67f7e8205b0306f84fac3af22d3
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-27 19:46:08 +0800

    feat: add test admin can create user with valid input

--- backend/tests/Feature/Entity/User/UserTest.php
+++ backend/tests/Feature/Entity/User/UserTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Entity\User;
+namespace Tests\Feature\User;
 
 use App\Enums\UserType;
 use App\Models\Role;
@@ -8,9 +8,66 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-use function PHPUnit\Framework\assertNotEquals;
-
 class UserTest extends TestCase
 {
-    
+    use RefreshDatabase;
+
+    /**
+     * Seed role.
+     * 
+     * Set up the test environment by seeding the database with roles.
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->artisan('db:seed', ['--class' => 'RoleSeeder']);
+        $this->artisan('db:seed', ['--class' => 'UserSeeder']);
+        $this->artisan('db:seed', ['--class' => 'CategorySeeder']);
+        $this->artisan('db:seed', ['--class' => 'SubCategorySeeder']);
+        $this->artisan('db:seed', ['--class' => 'PostSeeder']);
+    }
+
+    /**
+     * Test admin can store user with valid inputs via API.
+     * 
+     * This test verifies that an admin can store user with valid inputs via API endpoint.
+     */
+    public function test_admin_can_store_user_with_valid_inputs(): void
+    {
+        $role = Role::where('id', UserType::ADMINISTRATOR)->first();
+
+        if (!$role) {
+            $this->fail('Role Public User not found in the database.');
+        }
+
+        $this->get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN');
+
+        
+        $user = User::factory()->create([
+            'password' => bcrypt('password123'),
+            'role_id' => $role->id,
+        ]);
+
+        $this->get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN');
+
+        $this->post('/api/v1/authentication/login', [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+        
+        $response = $this->postJson(route('api.v1.users.store'), [
+            'email' => 'test000@gmail.com', 
+            'password' => 'password123', 
+            'password_confirmation' => 'password123', 
+            'role_id' => UserType::PUBLIC_USER
+        ]);
+
+        $response->assertCookie('laravel_session')
+                ->assertStatus(201)
+                ->assertJsonStructure(['message'])
+                ->assertJson(['message' => 'Successfully User Created.']);
+    }
 }
