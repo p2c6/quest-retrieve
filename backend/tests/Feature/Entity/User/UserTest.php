1f09a26a2a85c77be409b128e560e7456ec96aa3
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-28 10:43:59 +0800

    feat: add test other user type cannot update user

--- backend/tests/Feature/Entity/User/UserTest.php
+++ backend/tests/Feature/Entity/User/UserTest.php
@@ -316,4 +316,66 @@ class UserTest extends TestCase
                 'role_id',
         ]);
     }
+
+    /**
+     * Test other user type is unauthorize to update new user via API.
+     * 
+     * This test verifies that other user type is unauthorize to update new user via API endpoint.
+     */
+    public function test_other_user_type_is_unauthorize_to_update_user(): void
+    {
+        $role = Role::where('id', UserType::PUBLIC_USER)->first();
+
+        if (!$role) {
+            $this->fail('Role Public User not found in the database.');
+        }
+
+        $this->get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN');
+
+        
+        $user = User::factory()->create([
+            'password' => bcrypt('password123'),
+            'role_id' => $role->id,
+        ]);
+
+        $this->get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN');
+
+        $this->post('/api/v1/authentication/login', [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+        
+        
+        $originalEmail = 'test001@gmail.com';
+        $originalPassword = 'password1234';
+        $originalRole = UserType::MODERATOR;
+
+        $user = User::create([
+            'email' => $originalEmail,
+            'password' => $originalPassword,
+            'role_id' => $originalRole,
+        ]);
+
+        $updatedEmail = 'test000@gmail.com';
+        $updatedPassword = 'password123';
+        $updatedRole = UserType::PUBLIC_USER;
+        
+        $response = $this->putJson(route('api.v1.users.update', $user->id), [
+            'email' => $updatedEmail, 
+            'password' => $updatedPassword, 
+            'password_confirmation' => $updatedPassword, 
+            'role_id' => $updatedRole
+        ]);
+
+        $this->assertNotSame($originalEmail, $updatedEmail, 'Email must not equal to previous');
+        $this->assertNotSame($originalPassword, $updatedPassword, 'Password must not equal to previous');
+        $this->assertNotSame($originalRole, $updatedRole, 'Role must not equal to previous');
+
+        $response->assertCookie('laravel_session')
+                ->assertStatus(403)
+                ->assertJsonStructure(['message'])
+                ->assertJson(['message' => 'You are not allowed to access this action']);
+    }
 }
