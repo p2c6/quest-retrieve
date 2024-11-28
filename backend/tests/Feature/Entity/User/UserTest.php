440704cd1e512ef80c7624a383d82623d3dffb03
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-28 10:52:54 +0800

    feat: add test admin can delete user

--- backend/tests/Feature/Entity/User/UserTest.php
+++ backend/tests/Feature/Entity/User/UserTest.php
@@ -432,4 +432,48 @@ class UserTest extends TestCase
                 ]);
     }
     
+    /**
+     * Test admin can delete user via API.
+     * 
+     * This test verifies that an admin can delete user via API endpoint.
+     */
+    public function test_admin_can_delete_user(): void
+    {
+        $role = Role::where('id', UserType::ADMINISTRATOR)->first();
+
+        if (!$role) {
+            $this->fail('Role Public User not found in the database.');
+        }
+
+        $this->get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN');
+
+        
+        $user = User::factory()->create([
+            'password' => bcrypt('password123'),
+            'role_id' => $role->id,
+        ]);
+
+        $this->get('/sanctum/csrf-cookie')->assertCookie('XSRF-TOKEN');
+
+        $this->post('/api/v1/authentication/login', [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+
+        $newUser = User::create([
+            'email' => 'test000@gmail.com', 
+            'password' => 'password123', 
+            'password_confirmation' => 'password123', 
+            'role_id' => UserType::PUBLIC_USER
+        ]);
+        
+        $response = $this->deleteJson(route('api.v1.users.destroy', $newUser->id));
+
+        $response->assertCookie('laravel_session')
+                ->assertStatus(200)
+                ->assertJsonStructure(['message'])
+                ->assertJson(['message' => 'Successfully User Deleted.']);
+    }
 }
