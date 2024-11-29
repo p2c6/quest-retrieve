7b225a6eca41e5a5848fdafc2dc34b908f55b65d
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-29 21:58:01 +0800

    feat: update route implementation

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,41 +5,14 @@ import CreateView from '@/views/CreateView.vue'
 import LoginView from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import DashboardView from '@/views/DashboardView.vue'
+import guestRoutes from './guest';
+
+const routes = [
+  ...guestRoutes,
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView
-    },
-    {
-      path: '/contact/:id',
-      name: 'contact',
-      component: ContactView
-    },
-    {
-      path: '/item/create',
-      name: 'create',
-      component: CreateView
-    },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: DashboardView
-    },
-  ]
-})
-
+  routes,
+});
 export default router
