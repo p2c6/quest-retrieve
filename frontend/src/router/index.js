5ab095c39ea9c86dc798e6d5e815a880e4ecc02c
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-29 21:59:58 +0800

    chore: remove unuse import

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ContactView from '@/views/ContactView.vue'
-import CreateView from '@/views/CreateView.vue'
-import LoginView from '@/views/LoginView.vue'
-import RegisterView from '@/views/RegisterView.vue'
-import DashboardView from '@/views/DashboardView.vue'
 import guestRoutes from './guest';
 
 const routes = [
