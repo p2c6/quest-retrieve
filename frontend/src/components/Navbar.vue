8c507ab6149f319e20871063f9608cd9ba824de5
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-03 21:29:55 +0800

    feat: add authenticated user full name

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,11 +1,15 @@
 <script setup>
 import logo from "@/assets/qr-logo.png";
+import { useAuthStore } from "@/stores/auth";
 import { ref } from "vue";
 import { RouterLink } from "vue-router";
 
 const collapse = ref(false);
+const store = useAuthStore();
 
 const collapseNav = () => collapse.value = !collapse.value;
+
+
 
 </script>
 
@@ -16,25 +20,31 @@ const collapseNav = () => collapse.value = !collapse.value;
                 <button class="mt-2 border-[1.7px] rounded border-indigo-800 px-2 py-1 md:hidden" @click="collapseNav" id="btnCollapseNav">
                     <i class="text-indigo-800 pi pi-bars"></i>
                 </button>
-              
-                    <img :src="logo" alt="QuestRetrieve Logo" width="250px">
-              
-            </div>
 
+                <img :src="logo" alt="QuestRetrieve Logo" width="250px">
+            </div>
         </div>
         <div :class="`bg-indigo-800`">
-            <div :class="`mx-6 text-white flex flex-col p-2 gap-2 ${collapse ? 'block' : 'hidden'} md:flex md:flex-row md:justify-end`">
-                <div>
-                   
-                        Login
-                    
+                <div :class="`mx-6 text-white flex flex-col p-2 gap-2 ${collapse ? 'block' : 'hidden'} md:flex md:flex-row md:justify-end`" v-if="store.user?.first_name">
+                    <div>
+                        {{ store.user.first_name }}  {{ store.user.last_name }}
+                    </div>
+                    <div>
+                        Logout
+                    </div>
                 </div>
-                <div>
-                  
-                        Register
-                  
+                <div :class="`mx-6 text-white flex flex-col p-2 gap-2 ${collapse ? 'block' : 'hidden'} md:flex md:flex-row md:justify-end`" v-else>
+                    <div>
+                        <RouterLink :to="{name: 'login'}">
+                            Login
+                        </RouterLink>
+                    </div>
+                    <div>
+                        <RouterLink :to="{name: 'register'}">
+                            Register
+                        </RouterLink>
+                    </div>
                 </div>
-            </div>
             
         </div>
     </div>
