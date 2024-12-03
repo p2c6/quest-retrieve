d984f0c0f2c8cce249ec4ffeaa06bb0c093b0482
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-03 21:30:58 +0800

    feat: add initial route for user

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import guestRoutes from './guest';
+import LoginPage from "@/pages/authentication/LoginPage.vue";
+import RegisterPage from "@/pages/authentication/RegisterPage.vue";
+import HomePage from "@/pages/user/home/HomePage.vue";
+import UserLayout from '@/components/layouts/UserLayout.vue';
+import { useAuthStore } from "@/stores/auth.ts";
 
 const routes = [
-  ...guestRoutes,
+  {
+    path: "/",
+    component: UserLayout,
+    children: [
+      {
+        path: "",
+        name: "home",
+        component: HomePage,
+      },
+      {
+        path: "/login",
+        name: "login",
+        component: LoginPage,
+      },
+      {
+        path: "/register",
+        name: "register",
+        component: RegisterPage,
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 });
-export default router
+
+export default router;
