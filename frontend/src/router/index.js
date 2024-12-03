8c242210e0dde0c720b48038125068b09b2bef00
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-03 21:35:43 +0800

    ref: move route logic to user router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,32 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginPage from "@/pages/authentication/LoginPage.vue";
-import RegisterPage from "@/pages/authentication/RegisterPage.vue";
-import HomePage from "@/pages/user/home/HomePage.vue";
-import UserLayout from '@/components/layouts/UserLayout.vue';
-import { useAuthStore } from "@/stores/auth.ts";
+import userRoutes from "./user";
 
 const routes = [
-  {
-    path: "/",
-    component: UserLayout,
-    children: [
-      {
-        path: "",
-        name: "home",
-        component: HomePage,
-      },
-      {
-        path: "/login",
-        name: "login",
-        component: LoginPage,
-      },
-      {
-        path: "/register",
-        name: "register",
-        component: RegisterPage,
-      },
-    ],
-  },
+  ...userRoutes
 ];
 
 const router = createRouter({
---
+++ frontend/src/router/user.js
@@ -0,0 +1,28 @@
+import LoginPage from "@/pages/authentication/LoginPage.vue";
+import RegisterPage from "@/pages/authentication/RegisterPage.vue";
+import HomePage from "@/pages/user/home/HomePage.vue";
+import UserLayout from '@/components/layouts/UserLayout.vue';
+
+export default [
+  {
+    path: "/",
+    component: UserLayout,
+    children: [
+      {
+        path: "",
+        name: "home",
+        component: HomePage,
+      },
+      {
+        path: "/login",
+        name: "login",
+        component: LoginPage,
+      },
+      {
+        path: "/register",
+        name: "register",
+        component: RegisterPage,
+      },
+    ],
+  },
+];
