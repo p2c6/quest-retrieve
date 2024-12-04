467a70c5b7ec8260d0c25568a597a03349a1381b
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-04 16:51:38 +0800

    feat: add type user registrationb

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,4 +1,13 @@
-export type LoginCredentials  = {
+export type UserLogin  = {
     email: string;
     password: string;
+}
+
+export type UserRegistration = {
+    email: string;
+    password: string;
+    last_name: string;
+    first_name: string;
+    birthday: string;
+    contact_no: number;
 }
