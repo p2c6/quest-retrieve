088ae18641827aaba8812d4d25decb46a673c055
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-04 16:52:05 +0800

    feat: implement user registration

--- frontend/src/pages/authentication/RegisterPage.vue
+++ frontend/src/pages/authentication/RegisterPage.vue
@@ -3,7 +3,19 @@ import { RouterLink } from 'vue-router';
 import logo from "@/assets/qr-logo.png";
 import people from "@/assets/people.png";
 import Input from '@/components/Input.vue';
+import { useAuthStore } from '@/stores/auth';
+import { reactive } from 'vue';
 
+const authStore = useAuthStore();
+
+const formData = reactive({
+    email: '',
+    password: '',
+    last_name: '',
+    first_name: '',
+    birthday: '',
+    contact_no: '',
+});
 
 </script>
 
@@ -13,7 +25,6 @@ import Input from '@/components/Input.vue';
         <div class="mx-6 mt-6">
         
                 <i class="pi pi-arrow-left text-secondary"></i>
-         
             
             <div class="flex flex-col items-center mt-1">
                 <div>
@@ -21,7 +32,7 @@ import Input from '@/components/Input.vue';
                 </div>
 
                 <div class="container mx-auto w-auto mt-10">
-                    <form action="">
+                    <form @submit.prevent="authStore.register(formData)">
                         <div>
                             <div class="mb-5">
                                 <h1 class="text-primary font-medium text-lg">Create your account</h1>
@@ -30,30 +41,36 @@ import Input from '@/components/Input.vue';
 
                             <div>
                                 <label class="text-primary text-sm font-medium">Last Name</label>
-                                <Input  />
+                                <input type="text" v-model="formData.last_name" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                <p v-if="authStore.errors && authStore.errors.last_name" class="text-red-500 text-xs">{{ authStore.errors.last_name[0] }}</p>
                             </div>
                             <div>
                                 <label class="text-primary text-sm font-medium">First Name</label>
-                                <Input  />
+                                <input type="text" v-model="formData.first_name" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                <p v-if="authStore.errors && authStore.errors.first_name" class="text-red-500 text-xs">{{ authStore.errors.first_name[0] }}</p>
                             </div>
                         </div>
 
                             <div>
                                 <label class="text-primary text-sm font-medium">Contact Number</label>
-                                <Input  />
+                                <input type="text" v-model="formData.contact_no" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                <p v-if="authStore.errors && authStore.errors.contact_no" class="text-red-500 text-xs">{{ authStore.errors.contact_no[0] }}</p>
                             </div>
                             <div>
                                 <label class="text-primary text-sm font-medium">Date of birth</label>
                                 <br>
-                                <input type="date" class="h-8 w-full border-[1.1px] border-primary mt-2 mb-2 p-2 rounded"  />
+                                <input type="date" v-model="formData.birthday" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded"  />
+                                <p v-if="authStore.errors && authStore.errors.birthday" class="text-red-500 text-xs">{{ authStore.errors.birthday[0] }}</p>
                             </div>
                             <div>
                                 <label class="text-primary text-sm font-medium">Email</label>
-                                <Input  />
+                                <input type="text" v-model="formData.email" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                <p v-if="authStore.errors && authStore.errors.email" class="text-red-500 text-xs">{{ authStore.errors.email[0] }}</p>
                             </div>
                             <div>
                                 <label class="text-primary text-sm font-medium">Password</label>
-                                <Input  />
+                                <input type="password" v-model="formData.password" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                <p v-if="authStore.errors && authStore.errors.password" class="text-red-500 text-xs">{{ authStore.errors.password[0] }}</p>
                             </div>
                             <div class="border-t-[1.1px] border-gray w-full mt-4"></div>
                             <div class="mt-4 flex flex-row items-center gap-1">
--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia"
 import { apiClient } from "@/config/http";
-import type { UserLogin } from "@/types";
+import type { UserLogin, UserRegistration } from "@/types";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 
@@ -24,15 +24,38 @@ export const useAuthStore = defineStore('auth', () => {
         }
     }
 
-    const login = async(credentials: UserLogin): Promise<any> => {
+    const login = async(payload: UserLogin): Promise<any> => {
         isLoading.value = true;
         errors.value = null;
         try { 
-            const response = await apiClient.post('/authentication/login', credentials);
+            const response = await apiClient.post('/authentication/login', payload);
 
             if (response.status === 200) {
                 await getUser();
                 router.push({name: 'home'});
+            }
+
+        } catch(error: any) {
+            if (error.status === 422) {
+                console.log('Validation error', error);
+                errors.value = error.response.data.errors;
+                return;
+            }
+            
+            console.log("Login error: " , error);
+        } finally {
+            isLoading.value = false;
+        }
+    }
+
+    const register = async(payload: UserRegistration): Promise<any> => {
+        isLoading.value = true;
+        errors.value = null;
+        try { 
+            const response = await apiClient.post('/authentication/register', payload);
+
+            if (response.status === 201) {
+                console.log('resp', response)
             }
 
         } catch(error: any) {
@@ -81,6 +104,7 @@ export const useAuthStore = defineStore('auth', () => {
             @Functions
         */
         login,
+        register,
         logout,
         getUser,
     }
