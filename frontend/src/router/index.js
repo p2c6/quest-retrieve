41e4e73ab5e58871475a655bc749152f23ed6762
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-05 17:54:33 +0800

    ref: authenticated user

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,14 +15,14 @@ router.beforeEach(async (to, from, next) => {
   const authStore = useAuthStore();
 
   await authStore.getUser();
-
-  const notYetVerified = authStore.user && !authStore.user.email_verified_at;
+  const authenticatedUser = authStore.user;
+  const notYetVerified = authenticatedUser && !authenticatedUser.email_verified_at;
   const forbbidenRoutes = ['home', 'register', 'login'];
 
   if (notYetVerified && forbbidenRoutes.includes(to.name)) {
     return next({name: 'email.verification'})
   }
-
+  
   next();
 });
 
