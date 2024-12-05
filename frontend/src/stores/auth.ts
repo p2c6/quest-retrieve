66c8c91e5a947958b32551930d32dcdfc166d892
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-05 16:23:12 +0800

    feat: add redirection on email verification if not verified yet

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -32,6 +32,11 @@ export const useAuthStore = defineStore('auth', () => {
 
             if (response.status === 200) {
                 await getUser();
+
+                if (!user.value?.email_verified_at) {
+                    router.push({name: 'email.verification'});
+                }
+                
                 router.push({name: 'home'});
             }
 
