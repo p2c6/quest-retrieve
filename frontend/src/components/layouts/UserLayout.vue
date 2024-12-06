39b8be02a04f867bbe2924c53c818996905de364
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-06 16:48:35 +0800

    feat: add reset password on route exclude path

--- frontend/src/components/layouts/UserLayout.vue
+++ frontend/src/components/layouts/UserLayout.vue
@@ -9,7 +9,7 @@ const store = useAuthStore();
 const isLoading = ref(true);
 
 const showNavbar = computed(() => {
-    const excludedPaths = ["/login", "/register", "/email-verification", "/verify-email"];
+    const excludedPaths = ["/login", "/register", "/email-verification", "/verify-email", "/reset-password"];
     return !excludedPaths.includes(route.path);
 });
 
