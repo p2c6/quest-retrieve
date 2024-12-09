0f61896f79afef8db551250f4f56371056dcd566
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-09 13:35:37 +0800

    feat: add initial admin layout

---
+++ frontend/src/components/layouts/AdminLayout.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { RouterView, useRoute } from 'vue-router';
+import Navbar from "@/components/Navbar.vue";
+import { computed, onBeforeMount, onMounted, ref } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+import Sidebar from '@/components/Sidebar.vue';
+
+const route = useRoute();
+const store = useAuthStore();
+const isLoading = ref(true);
+
+const showNavbar = computed(() => {
+    const excludedPaths = [
+                            "/login", 
+                            "/register", 
+                            "/email-verification", 
+                            "/verify-email", 
+                            "/forgot-password",
+                            "/reset-password",
+                        ];
+
+    return !excludedPaths.includes(route.path);
+});
+
+onBeforeMount(async() => {
+    await store.getUser();
+    isLoading.value = false;
+})
+
+</script>
+
+<template>
+    <div v-if="isLoading">
+        Loading...
+    </div>
+    <div v-else>
+        <Sidebar />
+        <RouterView />
+    </div>
+</template>
