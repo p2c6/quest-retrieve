ad0f986d13f72b14ddefcddfb4a878153bb82d69
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-09 18:22:01 +0800

    feat: add initial sidebar content

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -3,7 +3,29 @@
 </script>
 
 <template>
-    <div>
-        this is sidebar
-    </div>
+        <div>
+            <ul class="text-white p-2 md:mt-12">
+                <li>
+                    Dashboard
+                </li>
+                <li>
+                    System Configuration
+                    <div>
+                        <ul>
+                            <li>Category</li>
+                            <li>Subcategory</li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    User Management
+                    <div>
+                        <ul>
+                            <li>Category</li>
+                            <li>Subcategory</li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
 </template>
