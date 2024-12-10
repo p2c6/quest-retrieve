bf0901b601009c822fd5718abf02669722ff5fb7
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-10 22:40:34 +0800

    feat: add collapse sub nav item

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,4 +1,18 @@
 <script setup>
+import { ref } from 'vue';
+
+const systemConfigurationSubItems = ref(false);
+const userManagementSubItems = ref(false);
+
+const collapseSubItems = (parent) => {
+    if (parent === "System Configuration") {
+        systemConfigurationSubItems.value = !systemConfigurationSubItems.value;
+    }
+
+    if (parent === "User Management") {
+        userManagementSubItems.value =  !userManagementSubItems.value;
+    }
+}
 
 </script>
 
@@ -12,24 +26,24 @@
                     </div>
                 </li>
                 <li class="my-2">
-                    <div class="list-item-parent">
+                    <div class="list-item-parent"  @click="collapseSubItems('System Configuration')">
                         <i class="pi pi-cog"></i>
                         <h4 id="list-main-item">System Configuration</h4>
                         <i class="pi pi-angle-down text-xs ml-auto"></i>
                     </div>
-                    <ul class="ml-8">
+                    <ul :class="`ml-8 ${systemConfigurationSubItems ? 'block' : 'hidden'}`">
                         <li>Category</li>
                         <li>Roles</li>
                         <li>Subcategory</li>
                     </ul>
                 </li>
                 <li class="my-2">
-                    <div class="list-item-parent">
+                    <div class="list-item-parent" @click="collapseSubItems('User Management')">
                         <i class="pi pi-user"></i>
                         <h4>User Management</h4>
                         <i class="pi pi-angle-down text-xs ml-auto"></i>
                     </div>
-                    <ul class="ml-8 hidden">
+                    <ul :class="`ml-8 ${userManagementSubItems ? 'block' : 'hidden'}`">
                         <li>Roles</li>
                         <li>Users</li>
                     </ul>
