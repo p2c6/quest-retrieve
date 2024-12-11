e69ac65bbe6ad8be120d32dd630eac78a8920ef3
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-11 23:37:55 +0800

    style: update width

--- frontend/src/pages/admin/system_configuration/category/CategoryPage.vue
+++ frontend/src/pages/admin/system_configuration/category/CategoryPage.vue
@@ -1,9 +1,22 @@
 <script setup>
+import Card from '@/components/Card.vue';
+import Table from '@/components/Table.vue';
+
 
 </script>
 
 <template>
-    <div>
-        This is category page
+    <div class="container grid grid-cols-1">
+        <div class="p-2">
+            <div>
+            </div>
+            <Card class="p-5 flex flex-row mt-2">
+                <div class="overflow-x-auto w-full">
+                    <p class="text-center md:text-left text-primary font-medium">Category List</p>
+                    <p class="text-tertiary text-center md:text-left text-xs md:text-sm">Listing of all categories.</p>
+                    <Table />
+                </div>
+            </Card>
+        </div>
     </div>
 </template>
