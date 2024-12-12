af8052e9c688b7e7250825b19ac4c41651ea8f27
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-12 17:49:31 +0800

    chore: update content; update structure

--- frontend/src/pages/admin/system_configuration/category/CreateCategoryPage.vue
+++ frontend/src/pages/admin/system_configuration/category/CreateCategoryPage.vue
@@ -1,7 +1,10 @@
 <script setup>
 import Card from '@/components/Card.vue';
 import Table from '@/components/Table.vue';
+import { useCategoryStore } from '@/stores/category';
 import { reactive } from 'vue';
+
+const categoryStore = useCategoryStore();
 
 const formData = reactive({
     name: ''
@@ -11,30 +14,28 @@ const formData = reactive({
 </script>
 
 <template>
-    <div class="container grid grid-cols-1">
-        <div class="p-2">
-            <Card class="p-5 flex flex-row mt-2">
-                <div class="overflow-x-auto w-full">
-                    <div class="flex justify-around">
-                        <div class="w-full">
-                            <p class="md:text-left text-primary font-medium">Create Category</p>
-                            <p class="text-tertiary md:text-left text-xs md:text-sm">Listing of all categories.</p>
-                        </div>
-                        <div>
-                            <button class="bg-secondary text-white px-2 py-1 rounded text-sm">Back to categories</button>
-                        </div>
+        <Card class="p-5 flex flex-row mt-2">
+            <div class="overflow-x-auto w-full">
+                <div class="flex justify-around">
+                    <div class="w-full">
+                        <p class="md:text-left text-primary font-medium">Create Category</p>
+                        <p class="text-tertiary md:text-left text-xs md:text-sm">Listing of all categories.</p>
                     </div>
                     <div>
-                        <form @submit.prevent="categoryStore.store(formData)">
-                            <div>
-                                <label class="text-primary text-sm font-medium">Category Name</label>
-                                <input type="text" v-model="formData.name" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
-                                <p v-if="categoryStore.errors && categoryStore.errors.name" class="text-red-500 text-xs">{{ categoryStore.errors.name[0] }}</p>
-                            </div>
-                        </form>
+                        <button class="bg-secondary text-white px-2 py-1 rounded text-sm">Back</button>
                     </div>
                 </div>
-            </Card>
-        </div>
-    </div>
+                <div class="mt-5">
+                    <form @submit.prevent="categoryStore.storeCategory(formData)">
+                        <div>
+                            <label class="text-primary text-sm font-medium">Category Name</label>
+                            <input type="text" v-model="formData.name" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                            <p v-if="categoryStore.errors && categoryStore.errors.name" class="text-red-500 text-xs">{{ categoryStore.errors.name[0] }}</p>
+                        </div>
+                        <button class="bg-secondary rounded-lg px-6 py-1 text-white mt-5 text-sm">Save</button>
+
+                    </form>
+                </div>
+            </div>
+        </Card>
 </template>
