89501e65037a03b66af1694cb50b1271f0d098a2
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-12 18:19:17 +0800

    feat: add loader

--- frontend/src/stores/category.ts
+++ frontend/src/stores/category.ts
@@ -54,6 +54,7 @@ export const useCategoryStore = defineStore('category', () => {
         categories,
         message,
         errors,
+        isLoading,
         getAllCategories,
         storeCategory,
     }
