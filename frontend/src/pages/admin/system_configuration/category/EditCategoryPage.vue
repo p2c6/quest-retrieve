59467c882a461883f1e69481aadd08bc468cb808
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-13 23:52:15 +0800

    style: update title and button responsiveness

--- frontend/src/pages/admin/system_configuration/category/EditCategoryPage.vue
+++ frontend/src/pages/admin/system_configuration/category/EditCategoryPage.vue
@@ -36,14 +36,16 @@ onUnmounted(() => {
 <template>
         <Card class="p-5 flex flex-row mt-2">
             <div class="overflow-x-auto w-full">
-                <div class="flex justify-around">
-                    <div class="w-full">
+                <div class="flex flex-col gap-2 justify-between items-center md:flex-row">
+                    <div class="text-center md:text-left">
                         <p class="md:text-left text-primary font-medium">Edit Category</p>
                         <p class="text-tertiary md:text-left text-xs md:text-sm">You are about to update category.</p>
                     </div>
-                    <div>
+                    <div class="w-full md:w-6">
                         <RouterLink :to="{name: 'category.list'}">
-                            <button class="bg-secondary text-white px-2 py-1 rounded text-sm">Back</button>
+                            <div class="bg-slate-200 rounded text-center">
+                                <i class="text-primary pi pi-chevron-left cursor-pointer text-xs"></i>
+                            </div>
                         </RouterLink>
                     </div>
                 </div>
@@ -54,7 +56,7 @@ onUnmounted(() => {
                             <input type="text" v-model="formData.name" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
                             <p v-if="categoryStore.errors && categoryStore.errors.name" class="text-red-500 text-xs">{{ categoryStore.errors.name[0] }}</p>
                         </div>
-                        <button class="bg-secondary rounded-lg px-6 py-1 text-white mt-5 text-sm">Update</button>
+                        <button class="bg-secondary rounded-lg px-6 py-1 text-white text-sm w-full mt-2 md:w-24">Update</button>
 
                     </form>
                 </div>
