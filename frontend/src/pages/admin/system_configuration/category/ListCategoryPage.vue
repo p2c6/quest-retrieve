d6f8c7c44b1617ee3b5ee2d676d338787e6e13aa
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-17 13:23:30 +0800

    feat: add conditional render when no data found

--- frontend/src/pages/admin/system_configuration/category/ListCategoryPage.vue
+++ frontend/src/pages/admin/system_configuration/category/ListCategoryPage.vue
@@ -131,7 +131,7 @@ onBeforeUnmount(() => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="category in categoryStore.categories.data" :key="category.id" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                            <tr v-if="categoryStore.categories.data.length > 0" v-for="category in categoryStore.categories.data" :key="category.id" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     {{ category.name }}
                                 </th>
@@ -145,6 +145,9 @@ onBeforeUnmount(() => {
                                         </div>
                                     </div>
                                 </td>
+                            </tr>
+                            <tr v-else>
+                                <td colspan="2" class="text-center">No data found.</td>
                             </tr>
                         </tbody>
                     </table>
