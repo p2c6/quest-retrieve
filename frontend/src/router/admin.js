9df698c43ae5aba7965b60c3facf5c7b636f41ab
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-18 16:42:53 +0800

    feat: add subcategory route

--- frontend/src/router/admin.js
+++ frontend/src/router/admin.js
@@ -4,6 +4,10 @@ import CategoryPage from "@/pages/admin/system_configuration/category/CategoryPa
 import ListCategoryPage from "@/pages/admin/system_configuration/category/ListCategoryPage.vue";
 import CreateCategoryPage from "@/pages/admin/system_configuration/category/CreateCategoryPage.vue";
 import EditCategoryPage from "@/pages/admin/system_configuration/category/EditCategoryPage.vue";
+import SubcategoryPage from '@/pages/admin/system_configuration/subcategory/SubcategoryPage.vue';
+import ListSubcategoryPage from '@/pages/admin/system_configuration/subcategory/ListSubcategoryPage.vue';
+import CreateSubcategoryPage from '@/pages/admin/system_configuration/subcategory/CreateSubcategoryPage.vue';
+import EditSubcategoryPage from '@/pages/admin/system_configuration/subcategory/EditSubcategoryPage.vue';
 
 export default [
   {
@@ -33,6 +37,27 @@ export default [
             path: "edit/:id",
             name: "category.edit",
             component: EditCategoryPage,
+          },
+        ],
+      },
+      {
+        path: "subcategory",
+        component: SubcategoryPage,
+        children: [
+          {
+            path: "",
+            name: "subcategory.list",
+            component: ListSubcategoryPage,
+          },
+          {
+            path: "create",
+            name: "subcategory.create",
+            component: CreateSubcategoryPage,
+          },
+          {
+            path: "edit/:id",
+            name: "subcategory.edit",
+            component: EditSubcategoryPage,
           },
         ],
       },
