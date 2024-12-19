ffb7b8044cf2475eb7c90983b53a914264da4b8c
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-19 19:21:47 +0800

    feat: add user page

---
+++ frontend/src/pages/admin/user_management/user/UserPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router';
+
+
+</script>
+
+<template>
+    <div class="relative container grid grid-cols-1 w-full">
+        <RouterView />
+    </div>
+</template>
