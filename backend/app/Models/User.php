618fa669c7c6aa5762b143c5922a77a13224f293
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-20 15:08:03 +0800

    feat: add email verified at in fillable column

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -27,6 +27,7 @@ class User extends Authenticatable implements MustVerifyEmail, CanResetPassword
         'name',
         'email',
         'password',
+        'email_verified_at',
         'role_id'
     ];
 
