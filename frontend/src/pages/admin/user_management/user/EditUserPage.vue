fe8aab92478493c7d6a4676cc37e58ae2982b18d
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-26 16:54:30 +0800

    feat: add edit user page

---
+++ frontend/src/pages/admin/user_management/user/EditUserPage.vue
@@ -0,0 +1,100 @@
+<script setup>
+import Card from '@/components/Card.vue';
+import { useRoleStore } from '@/stores/role';
+import { useUserStore } from '@/stores/user';
+import { onBeforeMount, reactive } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const userStore = useUserStore();
+const roleStore = useRoleStore();
+
+const formData = reactive({
+    email: '',
+    password: '',
+    password_confirmation: '',
+    last_name: '',
+    first_name: '',
+    birthday: '',
+    contact_no: '',
+    role_id: '',
+});
+
+onBeforeMount(async() => {
+    await roleStore.getAllRolesDropdown();
+})
+
+
+</script>
+
+<template>
+        <Card class="p-5 flex flex-row mt-2">
+            <div class="overflow-x-auto w-full">
+                <div class="flex flex-col gap-2 justify-between items-center md:flex-row">
+                    <div class="text-center md:text-left">
+                        <p class="md:text-left text-primary font-medium">Edit User</p>
+                        <p class="text-tertiary md:text-left text-xs md:text-sm">You are about to update user.</p>
+                    </div>
+                    <div class="w-full md:w-6">
+                        <RouterLink :to="{name: 'users.list'}">
+                            <div class="bg-slate-200 rounded text-center">
+                                <i class="text-primary pi pi-chevron-left cursor-pointer text-xs"></i>
+                            </div>
+                        </RouterLink>
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <form @submit.prevent="userStore.storeUser(formData)">
+                        <div>
+                            <div class="flex gap-y-0.5 flex-col md:flex-row gap-5">
+                                <div class="w-full md:w-1/2">
+                                    <label class="text-primary text-sm font-medium">Last Name</label>
+                                    <input type="text" v-model="formData.last_name" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                    <p v-if="userStore.errors && userStore.errors.last_name" class="text-red-500 text-xs">{{ userStore.errors.last_name[0] }}</p>
+                                </div>
+                                <div class="w-full md:w-1/2">
+                                    <label class="text-primary text-sm font-medium">First Name</label>
+                                    <input type="text" v-model="formData.first_name" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                    <p v-if="userStore.errors && userStore.errors.first_name" class="text-red-500 text-xs">{{ userStore.errors.first_name[0] }}</p>
+                                </div>
+                            </div>
+                            <div>
+                                <label class="text-primary text-sm font-medium">Date of birth</label>
+                                <br>
+                                <input type="date" v-model="formData.birthday" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded"  />
+                                <p v-if="userStore.errors && userStore.errors.birthday" class="text-red-500 text-xs">{{ userStore.errors.birthday[0] }}</p>
+                            </div>
+                            <div>
+                                <label class="text-primary text-sm font-medium">Contact Number</label>
+                                <input type="text" v-model="formData.contact_no" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                <p v-if="userStore.errors && userStore.errors.contact_no" class="text-red-500 text-xs">{{ userStore.errors.contact_no[0] }}</p>
+                            </div>
+                            <div>
+                                <label class="text-primary text-sm font-medium">Email</label>
+                                <input type="text" v-model="formData.email" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                <p v-if="userStore.errors && userStore.errors.email" class="text-red-500 text-xs">{{ userStore.errors.email[0] }}</p>
+                            </div>
+                            <div>
+                                <label class="text-primary text-sm font-medium">Password</label>
+                                <input type="password" v-model="formData.password" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                <p v-if="userStore.errors && userStore.errors.password" class="text-red-500 text-xs">{{ userStore.errors.password[0] }}</p>
+                            </div>
+                            <div>
+                                <label class="text-primary text-sm font-medium">Confirm Password</label>
+                                <input type="password" v-model="formData.password_confirmation" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                <p v-if="userStore.errors && userStore.errors.password" class="text-red-500 text-xs">{{ userStore.errors.password[0] }}</p>
+                            </div>
+                            <div>
+                            <label class="text-primary text-sm font-medium">Role</label>
+                            <select v-model="formData.role_id"  class="h-10 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                <option disabled value="">Please select role</option>
+                                <option v-for="role in roleStore.rolesDropdown" :value="role.id">{{  role.name }}</option>
+                            </select>
+                            <p v-if="userStore.errors && userStore.errors.role_id" class="text-red-500 text-xs">{{ userStore.errors.role_id[0] }}</p>
+                        </div>
+                            <button class="bg-secondary rounded-lg w-full py-1 text-white mt-5 text-sm md:w-20">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </Card>
+</template>
