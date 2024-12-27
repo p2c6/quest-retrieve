c75915266f9366bccd0d01dfca6098997c54929c
Archie Jefferson Em <archie.em99@gmail.com>
2024-12-27 18:11:22 +0800

    feat: add post initial page

---
+++ frontend/src/pages/user/posts/PostPage.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Filter from "@/components/Filter.vue";
+import CardList from "@/components/CardList.vue";
+import Card from "@/components/Card.vue";
+import NoItems from "@/components/NoItems.vue";
+import Post from "@/components/Post.vue";
+
+</script>
+
+<template>
+    <main>
+        <div class="mx-8 mt-2">
+            <RouterView />
+        </div>
+    </main>
+</template>
