c7bdcbf5a0fa1876dbfd32f3fac6cf76d65de215
Archie Jefferson Em <archie.em99@gmail.com>
2025-01-06 21:37:36 +0800

    feat: add test user cannot mark post as done if not owner

--- backend/tests/Feature/Post/PostTest.php
+++ backend/tests/Feature/Post/PostTest.php
@@ -1043,4 +1043,89 @@ class PostTest extends TestCase
                 ->assertJsonStructure(['message'])
                 ->assertJson(['message' => 'You are not allowed to access this action']);
     }
+
+    /**
+     * Test user cannot mark post as done if not owner via API.
+     * 
+     * This test verifies that a user cannot mark post as done if not owner via API endpoint.
+     */
+    public function test_user_cannot_mark_post_as_doneif_not_owner(): void
+    {
+        $role = Role::where('id', UserType::PUBLIC_USER)->first();
+
+        if (!$role) {
+            $this->fail('Role Public User not found in the database.');
+        }
+
+        $user = User::factory()->create([
+            'password' => bcrypt('password123'),
+            'role_id' => $role->id
+        ]);
+
+        $csrf = $this->get('/sanctum/csrf-cookie');
+
+        $csrf->assertCookie('XSRF-TOKEN');
+
+        $this->postJson('/api/v1/authentication/login', [
+            'email' => $user->email,
+            'password' => 'password123',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+
+        $category = Category::create([
+            'name' => "Sample Category",
+        ]);
+
+        $subCategory = Subcategory::create([
+            'category_id' => $category->id,
+            'name' => "Sample Subcategory",
+        ]);
+
+        $originalType = "Lost";
+        $originalSubCategoryId = $subCategory->id;
+        $originalIncidentLocation = 'Manila City';
+        $originalIncidentDate = '2024-01-02';
+
+        $post = Post::create([
+            'user_id' => $user->id,
+            'type' => $originalType,
+            'subcategory_id' => $originalSubCategoryId,
+            'incident_location' => $originalIncidentLocation,
+            'incident_date' => $originalIncidentDate,
+            'status' => PostStatus::PENDING
+        ]);
+
+        $otherPost = Post::create([
+            'user_id' => 1,
+            'type' => $originalType,
+            'subcategory_id' => $originalSubCategoryId,
+            'incident_location' => $originalIncidentLocation,
+            'incident_date' => $originalIncidentDate,
+            'status' => PostStatus::ON_PROCESSING
+        ]);
+
+        $updatedType = "Found";
+        $updatedSubCategoryId = 2;
+        $updatedIncidentLocation = 'Quezon City';
+        $updatedIncidentDate = '2024-05-02';
+
+        $response = $this->putJson(route('api.v1.posts.markAsDone', $otherPost->id), [
+            'user_id' => $user->id,
+            'type' => $updatedType,
+            'subcategory_id' => $updatedSubCategoryId,
+            'incident_location' => $updatedIncidentLocation,
+            'incident_date' => $updatedIncidentDate,
+        ]);
+
+        $this->assertNotSame($originalType, $updatedType, 'Type must not equal to previous');
+        $this->assertNotSame($originalSubCategoryId, $updatedSubCategoryId, 'Subcategory must not equal to previous');
+        $this->assertNotSame($originalIncidentLocation, $updatedIncidentLocation, 'Incident Location must not equal to previous');
+        $this->assertNotSame($originalIncidentDate, $updatedIncidentDate, 'Incident Date must not equal to previous');
+
+        $response->assertCookie('laravel_session')
+                ->assertStatus(403)
+                ->assertJsonStructure(['message'])
+                ->assertJson(['message' => 'You are not allowed to access this action']);
+    }
 }
