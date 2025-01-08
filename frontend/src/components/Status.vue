bb5142477d2006d2c010515100e242c9d56648ed
Archie Jefferson Em <archie.em99@gmail.com>
2025-01-09 03:06:57 +0800

    feat: add dynamic type prop value

--- frontend/src/components/Status.vue
+++ frontend/src/components/Status.vue
@@ -1,7 +1,9 @@
 <script setup>
-
+defineProps({
+    type: String
+})
 </script>
 
 <template>
-    <div class="rounded w-6 h-2 bg-red-500 place-self-end mb-3"></div>
+    <div class="rounded w-6 h-2 place-self-end mb-3" :class="`${type == 'Lost' ? 'bg-red-500' : 'bg-green-500'}`"></div>
 </template>
