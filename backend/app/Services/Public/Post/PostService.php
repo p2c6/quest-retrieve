a1afccf24467407827fa84d40f611d262ed0c816
Archie Jefferson Em <archie.em99@gmail.com>
2025-01-10 00:07:17 +0800

    feat: add claim functiion; add return function

--- backend/app/Services/Public/Post/PostService.php
+++ backend/app/Services/Public/Post/PostService.php
@@ -4,8 +4,11 @@ namespace App\Services\Public\Post;
 
 use App\Enums\PostStatus;
 use App\Filters\FilterPublicPost;
+use App\Mail\ClaimRequested;
+use App\Mail\ReturnRequested;
 use App\Models\Post;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Mail;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
@@ -40,5 +43,51 @@ class PostService
         ->appends($keyword);
                 
         return response()->json($posts);
+    }
+
+    /**
+     * Handle claim post request.
+     * 
+     * @param App\Models\Post $post The model of the post which needs to be retrieve.
+     * 
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function claim(Post $post, $request)
+    {
+        try {
+            Mail::to($post->user->email)->send(new ClaimRequested($post, $request));
+            
+            return response()->json([
+                'message' => 'Successfully Claim Requested.'
+            ], 200);
+        } catch (\Throwable $th) {
+            info("Error on claiming post: " . $th->getMessage());
+            return response()->json([
+                'message' => 'An error occurred during claiming post.'
+            ], 500);
+        }
+    }
+
+    /**
+     * Handle return post request.
+     * 
+     * @param App\Models\Post $post The model of the post which needs to be retrieve.
+     * 
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function return(Post $post, $request)
+    {
+        try {
+            Mail::to($post->user->email)->send(new ReturnRequested($post, $request));
+            
+            return response()->json([
+                'message' => 'Successfully Return Requested.'
+            ], 200);
+        } catch (\Throwable $th) {
+            info("Error on returning post: " . $th->getMessage());
+            return response()->json([
+                'message' => 'An error occurred during returning post.'
+            ], 500);
+        }
     }
 }
