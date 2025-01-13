0aeac5620ee1c4ef0a165744fae84af10cc6b08f
Archie Jefferson Em <archie.em99@gmail.com>
2025-01-13 23:49:57 +0800

    feat: add gate for get all subcategory

--- backend/app/Http/Controllers/API/v1/Admin/Entity/SubCategory/SubCategoryController.php
+++ backend/app/Http/Controllers/API/v1/Admin/Entity/SubCategory/SubCategoryController.php
@@ -11,6 +11,7 @@ use App\Models\Subcategory;
 use App\Services\Entity\Subcategory\SubCategoryService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class SubcategoryController extends Controller
 {
@@ -39,6 +40,10 @@ class SubcategoryController extends Controller
      */
     public function index(Request $request): JsonResponse
     {
+        if(Gate::denies('viewAny', Subcategory::class)) {
+            return response()->json(['message' => 'You are not allowed to access this action'], 403);
+        }
+        
         return $this->service->index($request->query('keyword'));
     }
 
