32fcb500cdbd8595b1d5fdc69b45dbb38bbb3cab
Archie Jefferson Em <archie.em99@gmail.com>
2025-01-14 03:03:22 +0800

    feat: fetch public post display

--- frontend/src/pages/user/home/HomePage.vue
+++ frontend/src/pages/user/home/HomePage.vue
@@ -1,49 +1,102 @@
 <script setup>
-import Navbar from "@/components/Navbar.vue";
-import Filter from "@/components/Filter.vue";
 import CardList from "@/components/CardList.vue";
 import Card from "@/components/Card.vue";
 import NoItems from "@/components/NoItems.vue";
 import Post from "@/components/Post.vue";
+import { usePostStore } from "@/stores/post";
+import { onBeforeMount, onBeforeUnmount, reactive, watch } from "vue";
+import { TailwindPagination } from 'laravel-vue-pagination';
 
-const datas = [
-{
-    id:1,
-    title: 'Test 1'
-},
-{
-    id:2,
-    title: 'Test 2'
-},
-{
-    id:3,
-    title: 'Test 3'
-},
-{
-    id:4,
-    title: 'Test 4'
-},
-{
-    id:5,
-    title: 'Test 5'
-},
-{
-    id:6,
-    title: 'Test 6'
-},
-]
+const postStore = usePostStore();
+
+
+const formData = reactive({
+    'keyword': ''
+})
+
+const search = async() => {
+    postStore.keyword = formData.keyword;
+    await postStore.getAllPublicPost();
+}
+
+const checkInput = async() => {
+    if (formData.keyword.trim() === '') {
+        postStore.keyword = null;
+        await postStore.getAllPublicPost();
+    }
+}
+
+onBeforeMount(async() => {
+    await postStore.getAllPublicPost();
+})
+
+onBeforeUnmount(() => {
+    postStore.keyword = null;
+    postStore.message = null;
+})
+
 </script>
 
 <template>
     <main>
         <div>
-            <Filter />
-            <CardList v-if="datas.length > 0">
-                <Card v-for="data in datas" :key="data.id">
-                    <Post />
+            <div class="mx-8 flex gap-2 mt-4">
+                <div class="w-screen">
+                    <div class="flex flex-col md:flex md:flex-row md:items-center gap-1">
+                        <input type="text" v-model="formData.keyword" @input="checkInput"  class="bg-white w-full p-2 rounded border-2 md:order-3 md:h-10 md:border-1" placeholder="I am looking for...">
+                        <button class="w-full bg-secondary mt-2 rounded md:w-10 md:h-10 md:mt-0" @click="search">
+                            <div class="flex flex-column p-2 justify-center gap-1 md:flex-col">
+                                <i class="pi pi-search text-white md:text-[1rem]"> </i> 
+                                <p class="text-white text-[0.8rem] md:hidden">Search</p>
+                            </div>
+                        </button>        
+                    </div>
+                    <div class="flex mt-4 gap-2 justify-center md:justify-end" v-if="!postStore.isLoading && postStore.posts.data && postStore.posts.data.length > 0">
+                        <div class="flex items-center justify-center gap-1">
+                            <div class="rounded-full w-5 h-3 bg-red-500"></div>
+                            <p class="text-xs text-gray-500 md:text-sm">Lost</p>
+                        </div>
+                        <div class="flex items-center justify-center gap-1">
+                            <div class="rounded-full w-5 h-3 bg-green-500"></div>
+                            <p class="text-xs text-gray-500 md:text-sm">Found</p>
+                        </div>
+                    </div>
+                    <div v-if="postStore.message" class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-5" role="alert">
+                        <div class="flex">
+                            <div class="py-1">
+                                <svg class="w-6 h-6 text-teal-500 dark:text-white mr-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M12 2c-.791 0-1.55.314-2.11.874l-.893.893a.985.985 0 0 1-.696.288H7.04A2.984 2.984 0 0 0 4.055 7.04v1.262a.986.986 0 0 1-.288.696l-.893.893a2.984 2.984 0 0 0 0 4.22l.893.893a.985.985 0 0 1 .288.696v1.262a2.984 2.984 0 0 0 2.984 2.984h1.262c.261 0 .512.104.696.288l.893.893a2.984 2.984 0 0 0 4.22 0l.893-.893a.985.985 0 0 1 .696-.288h1.262a2.984 2.984 0 0 0 2.984-2.984V15.7c0-.261.104-.512.288-.696l.893-.893a2.984 2.984 0 0 0 0-4.22l-.893-.893a.985.985 0 0 1-.288-.696V7.04a2.984 2.984 0 0 0-2.984-2.984h-1.262a.985.985 0 0 1-.696-.288l-.893-.893A2.984 2.984 0 0 0 12 2Zm3.683 7.73a1 1 0 1 0-1.414-1.413l-4.253 4.253-1.277-1.277a1 1 0 0 0-1.415 1.414l1.985 1.984a1 1 0 0 0 1.414 0l4.96-4.96Z" clip-rule="evenodd"/>
+                                </svg>
+                            </div>
+                            <div>
+                            <p class="font-bold text-sm">Success</p>
+                            <div class="text-xs flex gap-1">
+                                {{ postStore.message }}
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div role="status" v-if="postStore.isLoading" class="flex items-center justify-center mt-24">
+                <div class="flex flex-col items-center justify-center gap-5">
+                    <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                    </svg>
+                </div>
+            </div>
+            <CardList v-if="!postStore.isLoading && postStore.posts.data && postStore.posts.data.length > 0">
+                <Card v-for="post in postStore.posts.data" :key="post.id" >
+                    <Post :post="post" />
                 </Card>
             </CardList>
-            <NoItems v-else />
+            <TailwindPagination
+            class="flex items-center justify-center mt-10"
+                :data="postStore.posts"
+                @pagination-change-page="postStore.getAllPublicPost"
+            />
+            <NoItems v-if="!postStore.isLoading && postStore.posts.data && postStore.posts.data.length === 0" />
         </div>
     </main>
 </template>
