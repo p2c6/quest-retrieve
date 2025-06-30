9d789fc03bcf0425ca748046b0b665255046b1e3
Archie Jefferson Em <archie.em99@gmail.com>
2025-06-30 15:57:56 +0800

    chore: cleanup

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Profile;
-use App\Models\Role;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
