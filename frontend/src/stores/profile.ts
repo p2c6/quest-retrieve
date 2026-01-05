ad393b8e8b9c4fd755b26f952d70ccb1f390affb
Archie Jefferson Em <archie.em99@gmail.com>
2026-01-05 17:14:33 +0800

    feat: initial profile store

---
+++ frontend/src/stores/profile.ts
@@ -0,0 +1,89 @@
+import { apiClient } from "@/config/http";
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+
+export const useProfileStore = defineStore('profile', () => {
+    const router = useRouter();
+    const users = ref<object>({});
+    const isLoading = ref<boolean | null>(null);
+    const errors = ref<any | null>(null);
+    const message = ref(null);
+    const keyword = ref(null);
+    const column = ref<string | null>(null);
+
+    const updateProfileDetails = async(payload: any) => {
+        isLoading.value = false;
+
+        console.log(payload);
+
+        try {
+
+            if (!payload.password || payload.length === 0 || payload.password === '') {
+                delete payload.password;
+            }
+
+            const response = await apiClient.put(`/users/${payload.id}`, payload)
+
+            if (response.status === 200) {
+                message.value = response.data.message;
+                router.push({name: 'users.list'});
+            }
+
+        } catch(error: any) {
+            if (error.status == 422) {
+                console.log('Validation error', error);
+                errors.value = error.response.data.errors;
+                return;
+            }
+
+            console.log("Error on updating user: ", error)
+        }
+    }
+
+    const updateProfilePassword = async(payload: any) => {
+    isLoading.value = false;
+
+    try {
+
+        if (!payload.password || payload.length === 0 || payload.password === '') {
+            delete payload.password;
+        }
+
+        const response = await apiClient.put(`/users/${payload.id}`, payload)
+
+        if (response.status === 200) {
+            message.value = response.data.message;
+            router.push({name: 'users.list'});
+        }
+
+    } catch(error: any) {
+        if (error.status == 422) {
+            console.log('Validation error', error);
+            errors.value = error.response.data.errors;
+            return;
+        }
+
+        console.log("Error on updating user: ", error)
+    }
+}
+
+    return {
+        /*
+            @Variables
+         */
+        users,
+        errors,
+        message,
+        keyword,
+        isLoading,
+        column,
+
+        /*
+            @Functions
+        */
+        updateProfileDetails,
+        updateProfilePassword,
+    }
+});
