7ee50ff9dfe61c1459fa23f4a98caffe23563f60
Archie Jefferson Em <archie.em99@gmail.com>
2026-01-05 13:06:35 +0800

    feat: add yup validation

--- frontend/src/pages/user/posts/EditPostPage.vue
+++ frontend/src/pages/user/posts/EditPostPage.vue
@@ -5,6 +5,23 @@ import { useSubcategoryStore } from '@/stores/subcategory';
 import { usePostStore } from '@/stores/post';
 import { useRoute } from 'vue-router';
 
+import * as yup from 'yup';
+
+let schema = yup.object({
+    type: yup
+        .string()
+        .required('type is required.'),
+    subcategory_id: yup
+        .string()
+        .required('item is required.'),
+    incident_location: yup
+        .string()
+        .required('incident location is required.'),
+    incident_date: yup
+        .string()
+        .required('date of birth is required.'),
+})
+
 const route = useRoute();
 const post = ref({});
 
@@ -20,6 +37,28 @@ const formData = reactive({
 })
 
 const id = route.params?.id;
+
+const yupErrors = reactive({});
+
+const updatePost = async(formData) => {
+    yupErrors.type = '';
+    yupErrors.incident_location = '';
+    yupErrors.incident_date = '';
+    yupErrors.subcategory_id = '';
+
+    try {
+        await schema.validate(formData, {abortEarly: false })
+        postStore.updatePost(formData)
+    } catch(validationError) {
+        if (validationError.inner) {
+            validationError.inner.forEach(err => {
+                yupErrors[err.path] = err.message;
+            });
+        }
+
+        console.log('yuperrors', yupErrors)
+    }
+}
 
 onBeforeMount(async() => {
     await subcategoryStore.getAllSubcategoriesDropdown();
@@ -42,7 +81,7 @@ onMounted(async() => {
     <div class="mt-5 md:mt-5">
         <div class="container mx-auto grid grid-cols-1 place-items-start w-auto md:w-[420px]">
             <Card class="p-8">
-                    <form @submit.prevent="postStore.updatePost(formData)">
+                    <form @submit.prevent="updatePost(formData)">
                         <div class="border-b-2 mb-5">
                             <div class="flex flex-row items-center justify-center p-2 gap-x-2 text-primary">
                                 <i class="pi pi-plus-circle text-secondary"></i>
@@ -62,6 +101,7 @@ onMounted(async() => {
                                 </div>
                             </div>
                             <p v-if="postStore.errors && postStore.errors.type" class="text-red-500 text-xs">{{ postStore.errors.type[0] }}</p>
+                            <p v-else="yupErrors.type" class="text-red-500 text-xs">{{ yupErrors.type }}</p>
                         </div>
                         <div>
                             <label class="text-primary text-sm">Item</label>
@@ -70,17 +110,20 @@ onMounted(async() => {
                                 <option v-for="subcategory in subcategoryStore.subcategoriesDropdown" :value="subcategory.id">{{  subcategory.name }}</option>
                             </select>
                             <p v-if="postStore.errors && postStore.errors.subcategory_id" class="text-red-500 text-xs">{{ postStore.errors.subcategory_id[0] }}</p>
+                            <p v-else="yupErrors.subcategory_id" class="text-red-500 text-xs">{{ yupErrors.subcategory_id }}</p>
                         </div>
                         <div>
                             <label class="text-primary text-sm">Incident Location</label>
                             <input v-model="formData.incident_location" type="text"  class="h-6 w-full border-[1.1px] border-primary mt-1 mb-1 p-1 text-xs rounded md:h-10 p-2 text-base">
                             <p v-if="postStore.errors && postStore.errors.incident_location" class="text-red-500 text-xs">{{ postStore.errors.incident_location[0] }}</p>
+                            <p v-else="yupErrors.incident_location" class="text-red-500 text-xs">{{ yupErrors.incident_location }}</p>
                         </div>
 
                         <div>
                             <label class="text-primary text-sm">Incident Date</label>
                             <input type="date" v-model="formData.incident_date"  class="h-6 w-full border-[1.1px] border-primary mt-1 mb-1 p-1 text-xs rounded md:h-10 p-2 text-base">
                             <p v-if="postStore.errors && postStore.errors.incident_date" class="text-red-500 text-xs">{{ postStore.errors.incident_date[0] }}</p>
+                            <p v-else="yupErrors.incident_date" class="text-red-500 text-xs">{{ yupErrors.incident_date }}</p>
                         </div>
 
                         <button class="w-full bg-secondary p-1 mt-2 text-white rounded">Submit</button>
