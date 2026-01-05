94e1abfe0690c5a6571d673e54d63f5880e0ae22
Archie Jefferson Em <archie.em99@gmail.com>
2026-01-05 17:13:18 +0800

    feat: allow to fetch current user profile

--- frontend/src/pages/user/account/EdiitAccountPage.vue
+++ frontend/src/pages/user/account/EdiitAccountPage.vue
@@ -2,6 +2,9 @@
 import Card from '@/components/Card.vue';
 import { useRoleStore } from '@/stores/role';
 import { useUserStore } from '@/stores/user';
+import { useFileUploadStore } from '@/stores/file-upload';
+import { useAuthStore } from '@/stores/auth';
+import { useProfileStore } from '@/stores/profile';
 import { onBeforeMount, onMounted, onUnmounted, reactive, ref } from 'vue';
 import { RouterLink, useRoute } from 'vue-router';
 import * as yup from 'yup';
@@ -36,51 +39,47 @@ let schema = yup.object({
 const route = useRoute();
 
 const userStore = useUserStore();
-const roleStore = useRoleStore();
+const authStore = useAuthStore();
+const profileStore = useProfileStore();
 
 const user = ref({});
 
-const id = route.params?.id;
 
 const formData = reactive({
-    id: '',
-    email: '',
-    password: '',
-    password_confirmation: '',
-    last_name: '',
+    file: null,
     first_name: '',
-    birthday: '',
+    last_name: '',
     contact_no: '',
-    role_id: '',
+    avatar: '',
+    birthday: '',
 });
 
 const yupErrors = reactive({})
 
-const updateUser = async(formData) => {
-    yupErrors.last_name = '';
-    yupErrors.first_name = '';
-    yupErrors.birthday = '';
-    yupErrors.email = '';
-    yupErrors.password = '';
-    yupErrors.password_confirmation = '';
-    yupErrors.contact_no = '';
-    yupErrors.role_id = '';
-    userStore.errors = null;
+const updateUserProfile = async(formData) => {
+    profileStore.updateProfileDetails(formData)
+    // yupErrors.last_name = '';
+    // yupErrors.first_name = '';
+    // yupErrors.birthday = '';
+    // yupErrors.contact_no = '';
+    // userStore.errors = null;
 
-    try {
-        await schema.validate(formData, {abortEarly: false })
-        userStore.updateUser(formData)
-    } catch(validationError) {
-        if (validationError.inner) {
-            validationError.inner.forEach(err => {
-                yupErrors[err.path] = err.message;
-            });
-        }
-    }
+    // try {
+    //     await schema.validate(formData, {abortEarly: false })
+    //     userStore.updateUser(formData)
+    // } catch(validationError) {
+    //     if (validationError.inner) {
+    //         validationError.inner.forEach(err => {
+    //             yupErrors[err.path] = err.message;
+    //         });
+    //     }
+    // }
 }
 
 const fileInput = ref(null)
 const previewUrl = ref(null)
+const fileUploadStore = useFileUploadStore();
+
 
 const triggerFileInput = () => {
   fileInput.value.click()
@@ -88,30 +87,25 @@ const triggerFileInput = () => {
 
 const onFileChange = (event) => {
   const file = event.target.files[0]
-  if (file) {
-    previewUrl.value = URL.createObjectURL(file)
-  } else {
-    previewUrl.value = null
-  }
+
+  if (!file) return
+
+  previewUrl.value = URL.createObjectURL(file)
+  formData.file = file
+
+  const fd = new FormData()
+  fd.append('file', file)
+
+  fileUploadStore.uploadTemporaryFile(fd)
 }
 
-// onBeforeMount(async() => {
-//     await roleStore.getAllRolesDropdown();
-// })
-
 onMounted(async() => {
-    if (id) {
-        user.value = await userStore.getUser(id);
-
-        formData.id = id;
-        formData.email = user.value.email;
-        formData.last_name = user.value.last_name;
-        formData.first_name = user.value.first_name;
-        formData.birthday = user.value.birthday;
-        formData.contact_no = user.value.contact_no;
-        formData.role_id = user.value.role_id;
-    }
-})
+    authStore.getUser();
+    formData.last_name = authStore.user.profile.last_name;
+    formData.first_name = authStore.user.profile.first_name;
+    formData.contact_no = authStore.user.profile.contact_no;
+    formData.birthday = authStore.user.profile.birthday;
+});
 
 onUnmounted(() => {
     user.value = {};
@@ -140,7 +134,7 @@ onUnmounted(() => {
                             </div>
                         </div>
                         <div class="mt-5">
-                            <form @submit.prevent="updateUser(formData)">
+                            <form @submit.prevent="updateUserProfile(formData)">
                                 <div>
                                     <div class="flex justify-center">
                                         <div class="flex flex-col items-center space-y-4">
@@ -174,6 +168,16 @@ onUnmounted(() => {
                                             </div>
                                     </div>
                                     <div>
+                                            <div>
+                                                <label class="text-primary text-sm font-medium">Last Name</label>
+                                                <input type="text" v-model="formData.last_name" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                            </div>
+                                            <div>
+                                                <label class="text-primary text-sm font-medium">First Name</label>
+                                                <input type="text" v-model="formData.first_name" class="h-8 w-full border-[1.1px] border-primary mt-1 mb-1 p-2 rounded">
+                                            </div>
+                                    </div>
+                                    <div>
                                         <label class="text-primary text-sm font-medium">Date of birth</label>
                                         <br>
                                         <input type="date" v-model="formData.birthday" :class="`h-8 w-full border-[1.1px] border-${yupErrors.birthday || (userStore.errors && userStore.errors.birthday) ? 'red-500' : 'primary' } mt-1 mb-1 p-2 rounded`"  />
@@ -185,18 +189,6 @@ onUnmounted(() => {
                                         <input type="text" v-model="formData.contact_no" :class="`h-8 w-full border-[1.1px] border-${yupErrors.contact_no || (userStore.errors && userStore.errors.contact_no) ? 'red-500' : 'primary' } mt-1 mb-1 p-2 rounded`">
                                         <p v-if="yupErrors.contact_no" class="text-red-500 text-xs">{{ yupErrors.contact_no }}</p>
                                         <p v-else-if="userStore.errors && userStore.errors.contact_no" class="text-red-500 text-xs">{{ userStore.errors.contact_no[0] }}</p>
-                                    </div>
-                                    <div>
-                                        <label class="text-primary text-sm font-medium">Password</label>
-                                        <input type="password" v-model="formData.password" :class="`h-8 w-full border-[1.1px] border-${yupErrors.password || (userStore.errors && userStore.errors.password) ? 'red-500' : 'primary' } mt-1 mb-1 p-2 rounded`">
-                                        <p v-if="yupErrors.password" class="text-red-500 text-xs">{{ yupErrors.password }}</p>
-                                        <p v-else-if="userStore.errors && userStore.errors.password" class="text-red-500 text-xs">{{ userStore.errors.password[0] }}</p>
-                                    </div>
-                                    <div>
-                                        <label class="text-primary text-sm font-medium">Confirm Password</label>
-                                        <input type="password" v-model="formData.password_confirmation" :class="`h-8 w-full border-[1.1px] border-${yupErrors.password_confirmation || (userStore.errors && userStore.errors.password_confirmation) ? 'red-500' : 'primary' } mt-1 mb-1 p-2 rounded`">
-                                        <p v-if="yupErrors.password_confirmation" class="text-red-500 text-xs">{{ yupErrors.password_confirmation }}</p>
-                                        <p v-else-if="userStore.errors && userStore.errors.password" class="text-red-500 text-xs">{{ userStore.errors.password[0] }}</p>
                                     </div>
                                     <div>
                                 </div>
