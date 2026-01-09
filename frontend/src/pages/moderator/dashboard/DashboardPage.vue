4a9c1e6d0699ba6974cf4b71d78d3db168f0c5c2
Archie Jefferson Em <archie.em99@gmail.com>
2026-01-09 18:10:36 +0800

    feat: implement moderator dashboard posts count

--- frontend/src/pages/moderator/dashboard/DashboardPage.vue
+++ frontend/src/pages/moderator/dashboard/DashboardPage.vue
@@ -3,13 +3,20 @@ import PieChart from '@/components/PieChart.vue';
 import LineChart from '@/components/LineChart.vue';
 import Card from '@/components/Card.vue';
 import { onMounted } from "vue";
-import { useDashboardStore } from "@/stores/dashboard";
+import { useModeratorDashboardStore } from '@/stores/moderator/dashboard';
 
-const dashboardStore = useDashboardStore();
+const dashboardStore = useModeratorDashboardStore();
+const pieLabels = ['Pending', 'Approved', 'Rejected', 'Completed'];
+const backgroundColors = ['#e5eb36','#36eb78', '#eb3636', '#3694eb'];
 
 onMounted(async() => {
-    await dashboardStore.getUserCountPerMonth();
-    await dashboardStore.getVerifiedUserCount();
+    await dashboardStore.getPostLineData();
+    await dashboardStore.getPostPieData();
+
+    await dashboardStore.getTotalPostCount();
+    await dashboardStore.getApprovedPostCount();
+    await dashboardStore.getRejectedPostCount();
+    await dashboardStore.getCompletedPostCount();
 })
 
 </script>
@@ -26,7 +33,8 @@ onMounted(async() => {
                             <svg class="w-7 h-7 mb-3 text-body mt-4 text-blue-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M6 14h2m3 0h5M3 7v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1Z"/>
                             </svg>
-                            <h1 class="font-bold text-4xl my-2 text-blue-400">10</h1>
+                            <h1 v-if="dashboardStore.isLoading">...</h1>
+                            <h1 v-else class="font-bold text-4xl my-2 text-blue-400">{{ dashboardStore.totalPostCountData }}</h1>
                         </div>
                         <p class="text-body text-slate-400">The overall total number of posts</p>
                     </div>
@@ -40,9 +48,9 @@ onMounted(async() => {
                             <svg class="w-7 h-7 mb-3 text-body mt-4 text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8.032 12 1.984 1.984 4.96-4.96m4.55 5.272.893-.893a1.984 1.984 0 0 0 0-2.806l-.893-.893a1.984 1.984 0 0 1-.581-1.403V7.04a1.984 1.984 0 0 0-1.984-1.984h-1.262a1.983 1.983 0 0 1-1.403-.581l-.893-.893a1.984 1.984 0 0 0-2.806 0l-.893.893a1.984 1.984 0 0 1-1.403.581H7.04A1.984 1.984 0 0 0 5.055 7.04v1.262c0 .527-.209 1.031-.581 1.403l-.893.893a1.984 1.984 0 0 0 0 2.806l.893.893c.372.372.581.876.581 1.403v1.262a1.984 1.984 0 0 0 1.984 1.984h1.262c.527 0 1.031.209 1.403.581l.893.893a1.984 1.984 0 0 0 2.806 0l.893-.893a1.985 1.985 0 0 1 1.403-.581h1.262a1.984 1.984 0 0 0 1.984-1.984V15.7c0-.527.209-1.031.581-1.403Z"/>
                             </svg>
-                            <h1 class="font-bold text-4xl my-2 text-green-400">10</h1>
+                            <h1 v-if="dashboardStore.isLoading">...</h1>
+                            <h1 v-else class="font-bold text-4xl my-2 text-green-400">{{ dashboardStore.totalApprovedCountData }}</h1>
                         </div>
-                       
                         <p class="text-body text-slate-400">The overall total number of approved posts</p>
                     </div>
                 </Card>
@@ -52,10 +60,12 @@ onMounted(async() => {
                             <h5 class="mb-2 text-xl font-semibold tracking-tight text-heading text-red-400">Total Rejected Post</h5>
                         </a>
                         <div class="flex flex-row gap-2 items-center">
-                            <svg class="w-7 h-7 mb-3 text-body mt-4 text-red-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 21v-9m3-4H7.5a2.5 2.5 0 1 1 0-5c1.5 0 2.875 1.25 3.875 2.5M14 21v-9m-9 0h14v8a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-8ZM4 8h16a1 1 0 0 1 1 1v3H3V9a1 1 0 0 1 1-1Zm12.155-5c-3 0-5.5 5-5.5 5h5.5a2.5 2.5 0 0 0 0-5Z"/></svg>
-                            <h1 class="font-bold text-4xl my-2 text-red-400">10</h1>
+                            <svg class="w-7 h-7 text-red-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                            </svg>
+                            <h1 v-if="dashboardStore.isLoading">...</h1>
+                            <h1 v-else class="font-bold text-4xl my-2 text-red-400">{{ dashboardStore.totalRejectedCountData }}</h1>
                         </div>
-                       
                         <p class="text-body text-slate-400">The overall total number of rejected posts</p>
                     </div>
                 </Card>
@@ -65,8 +75,11 @@ onMounted(async() => {
                             <h5 class="mb-2 text-xl font-semibold tracking-tight text-heading text-primary">Total Completed Post</h5>
                         </a>
                         <div class="flex flex-row gap-2 items-center">
-                            <svg class="w-7 h-7 mb-3 text-body mt-4 text-primary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 21v-9m3-4H7.5a2.5 2.5 0 1 1 0-5c1.5 0 2.875 1.25 3.875 2.5M14 21v-9m-9 0h14v8a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-8ZM4 8h16a1 1 0 0 1 1 1v3H3V9a1 1 0 0 1 1-1Zm12.155-5c-3 0-5.5 5-5.5 5h5.5a2.5 2.5 0 0 0 0-5Z"/></svg>
-                            <h1 class="font-bold text-4xl my-2 text-primary">10</h1>
+                            <svg class="w-7 h-7 text-primary dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-6 7 2 2 4-4m-5-9v4h4V3h-4Z"/>
+                            </svg>
+                            <h1 v-if="dashboardStore.isLoading">...</h1>
+                            <h1 v-else class="font-bold text-4xl my-2 text-primary">{{ dashboardStore.totalCompletedCountData }}</h1>
                         </div>
                         <p class="text-body text-slate-400">The overall total number of completed posts</p>
                     </div>
@@ -81,10 +94,15 @@ onMounted(async() => {
                         </div>
                         <Card class="p-2 mb-5 pb-15 flex flex-col md:flex-row md:justify-around md:items-center md:gap-10 lg:gap-20">
                             <div class="w-full md:w-72 lg:w-[300px]">
-                                <LineChart />
+                            <LineChart :key="dashboardStore.postLineData.length" :data="dashboardStore.postLineData" />
                             </div>
                             <div class="w-full md:w-40 lg:w-72">
-                                <PieChart />
+                                <PieChart 
+                                    :key="dashboardStore.postPieData.length" 
+                                    :data="dashboardStore.postPieData" 
+                                    :labels="pieLabels"
+                                    :backgroundColors="backgroundColors" 
+                                />
                             </div>
                         </Card>
                     </div>
